adacb65d7c0300171ccff878255d23c9ca24d4cf
BozhidarN7 <bojonemski@gmail.com>
2020-12-04 13:12:05 +0200

    Regex-more exercise solved all

---
+++ Fundamentals/09. CSharp-Fundamentals-Regular-Expressions-Regex-More-Exercise/4.SantasSecretHelper/SantasSecretHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _4.SantasSecretHelper
+{
+    class SantasSecretHelper
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            string message = Console.ReadLine();
+            Regex pattern = new Regex(@"@(?<name>[A-Za-z]+)[^@\-!:>]*(?<behaviour>![GN]!)");
+            while (message != "end")
+            {
+                string decoded = new string(message.Select(x => x = (char)((int)x - key)).ToArray());
+                Match match = pattern.Match(decoded);
+                if (match.Groups["behaviour"].Value == "!G!")
+                {
+                    Console.WriteLine(match.Groups["name"].Value);
+                }
+                message = Console.ReadLine();
+            }
+        }
+    }
+}
