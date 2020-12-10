6e6f3f309902b58b680e5282b6cad5daf2ca04ce
BozhidarN7 <bojonemski@gmail.com>
2020-12-10 12:44:34 +0200

    Solved another problem

---
+++ Fundamentals/Archive/12.Programming-Fundamentals-Regular-Expressions-Regex-Exercise/5.КеyReplacer/КеyReplacer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _5.КеyReplacer
+{
+    class КеyReplacer
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex pattern = new Regex(@"^([A-Za-z]+)[\||<|\\](.+)[\||<|\\]([A-Za-z]+)");
+            Match match = pattern.Match(input);
+            string startKey = match.Groups[1].Value;
+            string endKey = match.Groups[3].Value;
+
+            string message = Console.ReadLine();
+            pattern = new Regex($@"{startKey}(?!{endKey})(.*?){endKey}");
+            MatchCollection matches = pattern.Matches(message);
+            string result = "";
+            foreach(Match item in matches)
+            {
+                result += item.Groups[1].Value;
+            }
+            if (result == string.Empty)
+            {
+                Console.WriteLine("Empty result");
+            }
+            else
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
