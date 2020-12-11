00077243fa4f5e5ae01ad0f518766680f3fc823e
BozhidarN7 <bojonemski@gmail.com>
2020-12-11 10:13:56 +0200

    Another problem solved

---
+++ Fundamentals/Archive/12.Programming-Fundamentals-Regular-Expressions-Regex-Exercise/7.QueryMess/QueryMess.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _7.QueryMess
+{
+    class QueryMess
+    {
+        static void Main(string[] args)
+        {
+            Regex pattern = new Regex(@"(?<key>[^?&]+)=(?<value>[^?&]+)");
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                MatchCollection matches = pattern.Matches(input);
+                Dictionary<string, List<string>> result = new Dictionary<string, List<string>>();
+                foreach (Match match in matches)
+                {
+                    string key = match.Groups["key"].Value;
+                    string value = match.Groups["value"].Value;
+
+                    Regex whiteSpacesPattern = new Regex(@"(%20|\+)+");
+                    key = whiteSpacesPattern.Replace(key, " ").Trim();
+                    value = whiteSpacesPattern.Replace(value, " ").Trim();
+                    if (!result.ContainsKey(key))
+                    {
+                        result.Add(key, new List<string>());
+                    }
+                    result[key].Add(value);
+                }
+
+                foreach (var item in result)
+                {
+                    Console.Write($"{item.Key}=[{string.Join(", ", item.Value)}]");
+                }
+                Console.WriteLine();
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
