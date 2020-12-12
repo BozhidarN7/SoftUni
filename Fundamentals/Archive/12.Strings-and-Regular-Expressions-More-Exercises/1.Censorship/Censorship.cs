984ebc99dd5d3a56fb9785ab1aa739d6b16005e1
BozhidarN7 <bojonemski@gmail.com>
2020-12-12 11:00:15 +0200

    Another problem solved

---
+++ Fundamentals/Archive/12.Strings-and-Regular-Expressions-More-Exercises/1.Censorship/Censorship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1.Censorship
+{
+    class Censorship
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string sentence = Console.ReadLine();
+
+            sentence = sentence.Replace(word, new string('*', word.Length));
+            Console.WriteLine(sentence);
+        }
+    }
+}
