0f6e02fa39472dcd7437b3bfd39a8f6e731b70ed
BozhidarN7 <bojonemski@gmail.com>
2020-12-12 11:35:29 +0200

    Another problem solved

---
+++ Fundamentals/Archive/12.Strings-and-Regular-Expressions-More-Exercises/3.KarateStrings/KarateStrings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace _3.KarateStrings
+{
+    class KarateStrings
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder result = new StringBuilder();
+            int totalStrength = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != '>')
+                {
+                    result.Append(input[i]);
+                    continue;
+                }
+                totalStrength += (input[i + 1] -'0');
+                result.Append(input[i]);
+                while (totalStrength > 0 && i < input.Length)
+                {
+                    i++;
+                    if (i < input.Length && input[i] == '>')
+                    {
+                        i--;
+                        break;
+                    }
+                    totalStrength--;
+                }
+            }
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
