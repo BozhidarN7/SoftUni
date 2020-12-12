90c7400beb80a12de9489503184ae93a262453a0
BozhidarN7 <bojonemski@gmail.com>
2020-12-12 11:07:38 +0200

    Another problem solved

--- Fundamentals/Archive/12.Strings-and-Regular-Expressions-More-Exercises/2.EmailMe/EmailMe.cs
+++ Fundamentals/Archive/12.Strings-and-Regular-Expressions-More-Exercises/2.EmailMe/EmailMe.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Linq;
 
 namespace _2.EmailMe
 {
-    class Program
+    class EmailMe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string email = Console.ReadLine();
+            int index = email.IndexOf("@");
+            string before = email.Substring(0, index);
+            string after = email.Substring(index + 1);
+
+            int difference = before.Sum(x => (int)x) - after.Sum(x => (int)x);
+            if (difference >= 0)
+            {
+                Console.WriteLine("Call her!");
+            }
+            else
+            {
+                Console.WriteLine("She is not the one.");
+            }
         }
     }
 }
