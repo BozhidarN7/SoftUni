bc5e6b3d0d75e9df0a390694cceb64937773eb60
BozhidarN7 <bojonemski@gmail.com>
2020-12-16 10:30:53 +0200

    Another problem solved

---
+++ Fundamentals/Archive/12.Strings-and-Regular-Expressions-More-Exercises/7.Hideout/Hideout.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _7.Hideout
+{
+    class Hideout
+    {
+        static void Main(string[] args)
+        {
+            string map = Console.ReadLine();
+            while (true)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                string character = Regex.Escape(tokens[0]);
+                int count = int.Parse(tokens[1]);
+
+                string pattern = $@"[{character}]+";
+
+                foreach (Match match in Regex.Matches(map, pattern))
+                {
+                    if (match.Length >= count)
+                    {
+                        Console.WriteLine($"Hideout found at index {match.Index} and it is with size {match.Length}!");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
