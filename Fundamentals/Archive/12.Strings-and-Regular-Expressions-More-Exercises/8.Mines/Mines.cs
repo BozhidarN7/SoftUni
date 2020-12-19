c9672b92d6e7affb5d23c881a3e672414d4848e2
BozhidarN7 <bojonemski@gmail.com>
2020-12-19 11:19:50 +0200

    Another problem solved

--- Fundamentals/Archive/12.Strings-and-Regular-Expressions-More-Exercises/8.Mines/Mines.cs
+++ Fundamentals/Archive/12.Strings-and-Regular-Expressions-More-Exercises/8.Mines/Mines.cs
@@ -10,22 +10,21 @@ namespace _8.Mines
             string input = Console.ReadLine();
             Regex pattern = new Regex(@"<[A-Za-z]{2}>");
 
-            string result = "";
             MatchCollection matches = pattern.Matches(input);
             foreach (Match match in matches)
             {
                 int index = match.Index;
-                int range = Math.Abs(input[index] - input[index + 1]);
+                int range = Math.Abs(input[index + 1] - input[index + 2]);
 
                 int toTheLeft = index - range >= 0 ? index - range : 0;
                 int toTheRight = index + range + 3 <= input.Length - 1 ? index + range + 3 : input.Length - 1;
 
-                result += input.Substring(0, toTheLeft);
-                result += new string('_', toTheRight - toTheLeft);
-                result += input.Substring(index + 4 + range);
+                string newPart = new string('_', toTheRight - toTheLeft + 1);
+                string replacementPart = input.Substring(toTheLeft, toTheRight - toTheLeft + 1);
+                input = input.Replace(replacementPart, newPart);
 
             }
-            Console.WriteLine(result);
+            Console.WriteLine(input);
         }
     }
 }
