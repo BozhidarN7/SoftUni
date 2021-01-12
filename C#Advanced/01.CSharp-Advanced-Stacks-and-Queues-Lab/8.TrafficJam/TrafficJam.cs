6cfdf9dbc8acd82288d0a042de143f8d44ea862c
BozhidarN7 <bojonemski@gmail.com>
2021-01-12 21:19:07 +0200

    Added last Stack and Queue problem from the lab

---
+++ C#Advanced/01.CSharp-Advanced-Stacks-and-Queues-Lab/8.TrafficJam/TrafficJam.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8.TrafficJam
+{
+    class TrafficJam
+    {
+        static void Main(string[] args)
+        {
+            int allowedToPass = int.Parse(Console.ReadLine());
+            Queue<string> cars = new Queue<string>();
+
+            string input = Console.ReadLine();
+            int totalCarsCrossed = 0;
+            while (input != "end")
+            {
+                if (input != "green")
+                {
+                    cars.Enqueue(input);
+                }
+                else
+                {
+                    for (int i = 0; i < allowedToPass; i++)
+                    {
+                        if (cars.Count != 0)
+                        {
+                            Console.WriteLine($"{cars.Dequeue()} passed!");
+                            totalCarsCrossed++;
+                        }
+                    }
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{totalCarsCrossed} cars passed the crossroads.");
+        }
+    }
+}
