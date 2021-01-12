c6cf98423a124c449464d4ca60c86520c8dc6603
BozhidarN7 <bojonemski@gmail.com>
2021-01-12 18:42:14 +0200

    Added Stack and Queue problem

---
+++ C#Advanced/01.CSharp-Advanced-Stacks-and-Queues-Lab/2.StackSum/StackSum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.StackSum
+{
+    class StackSum
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> st = new Stack<int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                st.Push(numbers[i]);
+            }
+            string command = Console.ReadLine();
+            while (command.ToLower() != "end")
+            {
+                string[] tokens = command.Split();
+                if (tokens[0].ToLower() == "add")
+                {
+                    st.Push(int.Parse(tokens[1]));
+                    st.Push(int.Parse(tokens[2]));
+                }
+                else if (tokens[0].ToLower() == "remove")
+                {
+                    if (int.Parse(tokens[1]) <= st.Count)
+                    {
+                        for (int i = 0; i < int.Parse(tokens[1]); i++)
+                        {
+                            st.Pop();
+                        } 
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Sum: {st.Sum()}");
+        }
+    }
+}
