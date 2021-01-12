8e17d7e8ee3cc4d0f6ab90180fee47f0133b3570
BozhidarN7 <bojonemski@gmail.com>
2021-01-12 20:47:48 +0200

    Added Stack and Queue problem

---
+++ C#Advanced/01.CSharp-Advanced-Stacks-and-Queues-Lab/7.HotPotato/HotPotato.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7.HotPotato
+{
+    class HotPotato
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> players = new Queue<string>(Console.ReadLine().Split());
+            int n = int.Parse(Console.ReadLine());
+
+            int currentMove = 1;
+            while (players.Count != 1)
+            {
+                if (currentMove != n)
+                {
+                    string currentPlayer = players.Dequeue();
+                    players.Enqueue(currentPlayer);
+                    currentMove++;
+                }
+                else
+                {
+                    Console.WriteLine($"Removed {players.Dequeue()}");
+                    currentMove = 1;
+                }
+            }
+            Console.WriteLine($"Last is {players.Dequeue()}");
+        }
+    }
+}
