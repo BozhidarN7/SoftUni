eac22c7173dd2ed7808943dad4a9f513dd011017
BozhidarN7 <bojonemski@gmail.com>
2021-01-12 20:38:17 +0200

    Added Stack and Queue problem

---
+++ C#Advanced/01.CSharp-Advanced-Stacks-and-Queues-Lab/6.Supermarket/Supermarket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6.Supermarket
+{
+    class Supermarket
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> names = new Queue<string>();
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input == "Paid")
+                {
+                    while (names.Count != 0)
+                    {
+                        Console.WriteLine(names.Dequeue());
+                    }
+                    continue;
+                }
+                names.Enqueue(input);
+            }
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+    }
+}
