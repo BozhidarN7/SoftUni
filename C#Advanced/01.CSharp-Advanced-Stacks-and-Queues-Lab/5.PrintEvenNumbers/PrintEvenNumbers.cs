6b1b62e4ae46d73e6fdd7fbb95e39db72760444f
BozhidarN7 <bojonemski@gmail.com>
2021-01-12 20:29:53 +0200

    Added Stack and Queue problem

---
+++ C#Advanced/01.CSharp-Advanced-Stacks-and-Queues-Lab/5.PrintEvenNumbers/PrintEvenNumbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _5.PrintEvenNumbers
+{
+    class PrintEvenNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ", Console.ReadLine().Split().Select(int.Parse).Where(x => x % 2 == 0).ToArray()));
+        }
+    }
+}
