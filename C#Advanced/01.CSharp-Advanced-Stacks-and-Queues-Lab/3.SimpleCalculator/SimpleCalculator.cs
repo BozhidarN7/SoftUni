aa4e7108969c27c7cf27f31b0cfa2a6980d1178f
BozhidarN7 <bojonemski@gmail.com>
2021-01-12 19:10:59 +0200

    Added Stack and Queue problem

---
+++ C#Advanced/01.CSharp-Advanced-Stacks-and-Queues-Lab/3.SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.SimpleCalculator
+{
+    class SimpleCalculator
+    {
+        static void Main(string[] args)
+        {
+            string[] expression = Console.ReadLine().Split();
+            Stack<string> st = new Stack<string>(expression.Reverse().ToArray());
+
+            while (st.Count != 1)
+            {
+                int firstNumber = int.Parse(st.Pop());
+                string operation = st.Pop();
+                int secondNumber = int.Parse(st.Pop());
+
+                if (operation == "+")
+                {
+                    st.Push((firstNumber + secondNumber).ToString());
+                }
+                else
+                {
+                    st.Push((firstNumber - secondNumber).ToString());
+                }
+            }
+            Console.WriteLine(st.Pop());
+        }
+    }
+}
