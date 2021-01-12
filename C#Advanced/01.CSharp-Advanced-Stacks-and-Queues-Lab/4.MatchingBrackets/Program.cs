e4783846c511972329befce68d97cb1d1b276358
BozhidarN7 <bojonemski@gmail.com>
2021-01-12 20:19:22 +0200

    Added Stack and Queue problem

---
+++ C#Advanced/01.CSharp-Advanced-Stacks-and-Queues-Lab/4.MatchingBrackets/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            Stack<int> indexes = new Stack<int>();
+
+            for (int i = 0; i< expression.Length; i++)
+            {
+                if (expression[i]== '(')
+                {
+                    indexes.Push(i);
+                }
+                if (expression[i] == ')')
+                {
+                    int index = indexes.Pop();
+                    string currentExpression = expression.Substring(index, i - index + 1);
+                    Console.WriteLine(currentExpression);
+                }
+            }
+        }
+    }
+}
