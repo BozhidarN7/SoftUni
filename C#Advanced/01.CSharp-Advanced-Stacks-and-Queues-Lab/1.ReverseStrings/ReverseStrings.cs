ac21166d01346c49e3e822367063dcec5fbc402f
BozhidarN7 <bojonemski@gmail.com>
2021-01-12 18:28:10 +0200

    Another Stack and Queue problem solved

---
+++ C#Advanced/01.CSharp-Advanced-Stacks-and-Queues-Lab/1.ReverseStrings/ReverseStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1.ReverseStrings
+{
+    class ReverseStrings
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            Stack<char> reversedStr = new Stack<char>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                reversedStr.Push(str[i]);
+            }
+            while(reversedStr.Count != 0)
+            {
+                Console.Write(reversedStr.Pop());
+            }
+        }
+    }
+}
