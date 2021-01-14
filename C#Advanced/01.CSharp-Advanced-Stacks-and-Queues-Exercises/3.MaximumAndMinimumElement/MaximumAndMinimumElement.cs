31e5c78bd50c78ead4d9b1ac0c55f1795ffacb88
BozhidarN7 <bojonemski@gmail.com>
2021-01-14 14:46:49 +0200

    Add Stack and Queue problem

---
+++ C#Advanced/01.CSharp-Advanced-Stacks-and-Queues-Exercises/3.MaximumAndMinimumElement/MaximumAndMinimumElement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.MaximumAndMinimumElement
+{
+    class MaximumAndMinimumElement
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            Stack<int> st = new Stack<int>();
+            for (int i = 0; i < N; i++)
+            {
+                int[] tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                if (tokens[0] == 1)
+                {
+                    st.Push(tokens[1]);
+                }
+                else if (tokens[0] == 2 && st.Count != 0)
+                {
+                    st.Pop();
+                }
+                else if (tokens[0] == 3 && st.Count != 0)
+                {
+                    Console.WriteLine(st.Max());
+                }
+                else if (tokens[0] == 4 && st.Count != 0)
+                {
+                    Console.WriteLine(st.Min());
+                }
+            }
+            Console.WriteLine(string.Join(", ", st));
+        }
+    }
+}
