3377bf0b1b40bc36628ef0fdfa44f3c8be91538d
BozhidarN7 <bojonemski@gmail.com>
2021-01-14 15:29:10 +0200

    Added Stack and Queue problem

---
+++ C#Advanced/01.CSharp-Advanced-Stacks-and-Queues-Exercises/4.FastFood/FastFood.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4.FastFood
+{
+    class FastFood
+    {
+        static void Main(string[] args)
+        {
+            int quantity = int.Parse(Console.ReadLine());
+            Queue<int> orders = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Console.WriteLine(orders.Max());
+            while (orders.Count != 0)
+            {
+                if (orders.Peek() <= quantity)
+                {
+                    quantity -= orders.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (orders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+            }
+        }
+    }
+}
