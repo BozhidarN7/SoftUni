aabd51b09a66e60d0484386f16f8c0fed47b3c50
BozhidarN7 <bojonemski@gmail.com>
2021-01-14 15:29:36 +0200

    Added Stack and Queue problem

---
+++ C#Advanced/01.CSharp-Advanced-Stacks-and-Queues-Exercises/5.FashionBoutique/FashionBoutique.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.FashionBoutique
+{
+    class FashionBoutique
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothesValues = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            int rackcNeeded = 0;
+            int spaceTakenSoFar = 0;
+            while (clothesValues.Count != 0)
+            {
+                if (spaceTakenSoFar + clothesValues.Peek() <= rackCapacity)
+                {
+                    spaceTakenSoFar += clothesValues.Pop();
+                }
+                else
+                {
+                    rackcNeeded++;
+                    spaceTakenSoFar = 0;
+                }
+            }
+            if (spaceTakenSoFar != 0)
+            {
+                rackcNeeded++;
+            }
+            Console.WriteLine(rackcNeeded);
+        }
+    }
+}
