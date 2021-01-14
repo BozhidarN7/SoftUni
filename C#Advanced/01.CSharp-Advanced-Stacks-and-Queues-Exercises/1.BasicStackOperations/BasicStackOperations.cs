5b996056b2174d886200c06a3fb76474e870da55
BozhidarN7 <bojonemski@gmail.com>
2021-01-14 14:25:19 +0200

    Add Stack and Queue problem

---
+++ C#Advanced/01.CSharp-Advanced-Stacks-and-Queues-Exercises/1.BasicStackOperations/BasicStackOperations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.BasicStackOperations
+{
+    class BasicStackOperations
+    {
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int N = tokens[0];
+            int S = tokens[1];
+            int X = tokens[2];
+
+            Stack<int> numbers = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            while (numbers.Count > 0 && S > 0)
+            {
+                numbers.Pop();
+                S--;
+            }
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (numbers.Contains(X))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(numbers.Min());
+            }
+        }
+    }
+}
