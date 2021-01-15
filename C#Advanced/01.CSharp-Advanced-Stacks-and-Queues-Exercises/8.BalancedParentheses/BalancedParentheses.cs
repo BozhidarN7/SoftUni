50dea07d7959a9f07bc6203671df77244723ed9c
BozhidarN7 <bojonemski@gmail.com>
2021-01-15 08:34:24 +0200

    Added Stack and Queue problem

---
+++ C#Advanced/01.CSharp-Advanced-Stacks-and-Queues-Exercises/8.BalancedParentheses/BalancedParentheses.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8.BalancedParentheses
+{
+    class BalancedParentheses
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+
+            bool isBalanced = IsBalanced(expression);
+
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+
+        private static bool IsBalanced(string expression)
+        {
+            Stack<char> openBrackets = new Stack<char>();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (openBrackets.Count == 0 && (expression[i] == ')' || expression[i] == '}' || expression[i] == ']'))
+                {
+                    return false;
+                }
+
+                if (expression[i] == '(' || expression[i] == '{' || expression[i] == '[')
+                {
+                    openBrackets.Push(expression[i]);
+                }
+                else
+                {
+                    if (expression[i] == ')' && openBrackets.Peek() != '(')
+                    {
+                        return false;
+                    }
+                    else if (expression[i] == '}' && openBrackets.Peek() != '{')
+                    {
+                        return false;
+                    }
+                    else if (expression[i] == ']' && openBrackets.Peek() != '[')
+                    {
+                        return false;
+                    }
+                    else if (expression[i] == ')' && openBrackets.Peek() == '(')
+                    {
+                        openBrackets.Pop();
+                    }
+                    else if (expression[i] == '}' && openBrackets.Peek() == '{')
+                    {
+                        openBrackets.Pop();
+                    }
+                    else if (expression[i] == ']' && openBrackets.Peek() == '[')
+                    {
+                        openBrackets.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
