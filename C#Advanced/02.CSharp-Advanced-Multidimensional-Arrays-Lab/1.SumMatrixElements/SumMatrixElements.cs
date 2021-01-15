456d08d4ffac32b24d585019ff9d283f4f3fa51d
BozhidarN7 <bojonemski@gmail.com>
2021-01-15 18:35:40 +0200

    Added Multidimensional arrays problem

---
+++ C#Advanced/02.CSharp-Advanced-Multidimensional-Arrays-Lab/1.SumMatrixElements/SumMatrixElements.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.SumMatrixElements
+{
+    class SumMatrixElements
+    {
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = tokens[0];
+            int cols = tokens[1];
+            int[,] matrix = new int[rows, cols];
+
+            int sum = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                int[] row = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = row[j];
+                    sum += matrix[i, j];
+                }
+            }
+
+          
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+        }
+    }
+}
