b1d7468ca6b8d5b9f689ce457a622ebd6c8b6109
BozhidarN7 <bojonemski@gmail.com>
2021-01-15 08:59:58 +0200

    Added Stack and Queue problem

---
+++ C#Advanced/01.CSharp-Advanced-Stacks-and-Queues-Exercises/9.SimpleTextEditor/SimpleTextEditor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _9.SimpleTextEditor
+{
+    class SimpleTextEditor
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<string> lastState = new Stack<string>();
+            string str = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                if (tokens[0] == "1")
+                {
+                    str += tokens[1];
+                    lastState.Push(str);
+                }
+                else if (tokens[0] == "2")
+                {
+                    str = str.Substring(0, str.Length - int.Parse(tokens[1]));
+                    lastState.Push(str);
+                }
+                else if (tokens[0] == "3")
+                {
+                    Console.WriteLine(str[int.Parse(tokens[1]) - 1]);
+                }
+                else
+                {
+                    lastState.Pop();
+                    if (lastState.Count != 0)
+                    {
+                        str = lastState.Peek();
+                    }
+                    else
+                    {
+                        str = string.Empty;
+                    }
+                }
+            }
+        }
+    }
+}
