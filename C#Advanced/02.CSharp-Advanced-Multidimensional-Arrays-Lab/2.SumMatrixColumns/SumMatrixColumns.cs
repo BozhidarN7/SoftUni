ecd8677035340cb2078fdb70cc44dbfe04649648
BozhidarN7 <bojonemski@gmail.com>
2021-01-15 18:36:36 +0200

    Added Multidimensional arrays problem

---
+++ C#Advanced/02.CSharp-Advanced-Multidimensional-Arrays-Lab/2.SumMatrixColumns/SumMatrixColumns.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2.SumMatrixColumns
+{
+    class SumMatrixColumns.
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
