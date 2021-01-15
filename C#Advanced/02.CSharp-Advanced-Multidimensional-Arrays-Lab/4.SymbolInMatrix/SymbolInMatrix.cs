835d0faebdcbefe3331cc32b06a5c04fd259f0ce
BozhidarN7 <bojonemski@gmail.com>
2021-01-15 19:06:09 +0200

    Added Multidimensional arrays prblem

---
+++ C#Advanced/02.CSharp-Advanced-Multidimensional-Arrays-Lab/4.SymbolInMatrix/SymbolInMatrix.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _4.SymbolInMatrix
+{
+    class SymbolInMatrix
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                string row = Console.ReadLine();
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = row[j];
+                }
+            }
+            char symbol = char.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i, j] == symbol)
+                    {
+                        Console.WriteLine($"({i}, {j})");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
