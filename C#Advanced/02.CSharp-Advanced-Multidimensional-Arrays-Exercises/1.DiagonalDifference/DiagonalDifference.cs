23658acd2f77f73a8583a0d8a91ffc26a0e0dc7d
BozhidarN7 <bojonemski@gmail.com>
2021-01-18 08:11:51 +0200

    Added Multidimensional arrays problem

---
+++ C#Advanced/02.CSharp-Advanced-Multidimensional-Arrays-Exercises/1.DiagonalDifference/DiagonalDifference.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.DiagonalDifference
+{
+    class DiagonalDifference
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] row = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = row[j];
+                }
+            }
+
+            int primaryDiagonalSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                primaryDiagonalSum += matrix[i, i];
+            }
+            int secondaryDiagonalSum = 0;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                secondaryDiagonalSum += matrix[n -i - 1, i];
+            }
+
+            Console.WriteLine(Math.Abs(primaryDiagonalSum - secondaryDiagonalSum));
+        }
+    }
+}
+
