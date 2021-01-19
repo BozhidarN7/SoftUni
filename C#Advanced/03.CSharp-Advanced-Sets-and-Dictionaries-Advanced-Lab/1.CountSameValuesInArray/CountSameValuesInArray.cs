cf1eb1b6d4dc8cf674d2ea9f48e082d47691f85a
BozhidarN7 <bojonemski@gmail.com>
2021-01-19 18:32:34 +0200

    Added Sets and Dictionary problem

---
+++ C#Advanced/03.CSharp-Advanced-Sets-and-Dictionaries-Advanced-Lab/1.CountSameValuesInArray/CountSameValuesInArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.CountSameValuesInArray
+{
+    class CountSameValuesInArray
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Dictionary<double, int> dic = new Dictionary<double, int>();
+
+            foreach(double number in numbers)
+            {
+                if (!dic.ContainsKey(number))
+                {
+                    dic.Add(number, 0);
+                }
+                dic[number]++;
+            }
+
+            foreach(var pair in dic)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value} times");
+            }
+        }
+    }
+}
