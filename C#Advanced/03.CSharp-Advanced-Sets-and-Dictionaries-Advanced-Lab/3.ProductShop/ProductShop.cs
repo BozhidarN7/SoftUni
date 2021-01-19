915d1c7930812deefe02cdf187ddd59c3bd8afb9
BozhidarN7 <bojonemski@gmail.com>
2021-01-19 19:46:30 +0200

    Added Sets and Dictionary problem

---
+++ C#Advanced/03.CSharp-Advanced-Sets-and-Dictionaries-Advanced-Lab/3.ProductShop/ProductShop.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3.ProductShop
+{
+    class ProductShop
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, Dictionary<string, double>> shops = new SortedDictionary<string, Dictionary<string, double>>();
+            string input = Console.ReadLine();
+            while(input != "Revision")
+            {
+                string[] tokens = input.Split(", ");
+                string shop = tokens[0];
+                string product = tokens[1];
+                double price = double.Parse(tokens[2]);
+
+                if (!shops.ContainsKey(shop))
+                {
+                    shops.Add(shop, new Dictionary<string, double>());
+                }
+                if (!shops[shop].ContainsKey(product))
+                {
+                    shops[shop].Add(product, price);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach(var shop in shops)
+            {
+                Console.WriteLine($"{shop.Key}->");
+                foreach(var product in shop.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
