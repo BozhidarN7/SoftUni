ad462ef94215c04aed8f2b443ddb5e67dab5b211
BozhidarN7 <bojonemski@gmail.com>
2021-01-20 13:36:15 +0200

    Added Sets and Dictionary problem

---
+++ C#Advanced/03.CSharp-Advanced-Sets-and-Dictionaries-Advanced-Lab/5.RecordUniqueNames/RecordUniqueNames.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5.RecordUniqueNames
+{
+    class RecordUniqueNames
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+            Console.WriteLine(string.Join("\n", names));
+        }
+    }
+}
