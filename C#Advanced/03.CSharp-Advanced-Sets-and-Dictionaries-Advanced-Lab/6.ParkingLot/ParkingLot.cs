ed5a0873ee68c4224864b1a9b595418397814e84
BozhidarN7 <bojonemski@gmail.com>
2021-01-20 13:43:00 +0200

    Added Sets and Dictionary problem

---
+++ C#Advanced/03.CSharp-Advanced-Sets-and-Dictionaries-Advanced-Lab/6.ParkingLot/ParkingLot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6.ParkingLot
+{
+    class ParkingLot
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> cars = new HashSet<string>();
+            string input = Console.ReadLine();
+            while(input != "END")
+            {
+                string[] tokens = input.Split(", ");
+                if (tokens[0] == "IN")
+                {
+                    cars.Add(tokens[1]);
+                }
+                else
+                {
+                    cars.Remove(tokens[1]);
+                }
+                input = Console.ReadLine();
+            }
+            if (cars.Count != 0)
+            {
+                Console.WriteLine(string.Join("\n",cars));
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
