ed48b118943d5d91a7f1537751e9c258e07fcbc7
BozhidarN7 <bojonemski@gmail.com>
2021-01-21 19:08:01 +0200

    Added Sets and Dictionaries problem

---
+++ C#Advanced/03.CSharp-Advanced-Sets-and-Dictionaries-Advanced-Exercise/4.EvenTimes/EvenTimes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4.EvenTimes
+{
+    class EvenTimes
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<int, int> elements = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                int element = int.Parse(Console.ReadLine());
+                if (!elements.ContainsKey(element))
+                {
+                    elements.Add(element, 0);
+                }
+                elements[element]++;
+            }
+
+            Console.WriteLine(elements.Where(x => x.Value % 2 == 0).ToArray()[0].Key);
+        }
+    }
+}
