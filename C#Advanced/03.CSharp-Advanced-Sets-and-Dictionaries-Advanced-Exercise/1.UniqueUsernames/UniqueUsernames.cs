c60ad5025b531d38d7a32cde8bcbad163bd5cf5a
BozhidarN7 <bojonemski@gmail.com>
2021-01-21 18:33:08 +0200

    Added Sets and Dictionaries problem

---
+++ C#Advanced/03.CSharp-Advanced-Sets-and-Dictionaries-Advanced-Exercise/1.UniqueUsernames/UniqueUsernames.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1.UniqueUsernames
+{
+    class UniqueUsernames
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i =0; i < n; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+            Console.WriteLine(string.Join("\n", names));
+        }
+    }
+}
