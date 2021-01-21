5ae5b1213b2c868bfdf8fdf4f64bdb28ae63e94b
BozhidarN7 <bojonemski@gmail.com>
2021-01-21 18:54:28 +0200

    Added Sets and Dictionaries problem

---
+++ C#Advanced/03.CSharp-Advanced-Sets-and-Dictionaries-Advanced-Exercise/3.PeriodicTable/PeriodicTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.PeriodicTable
+{
+    class PeriodicTable
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> elements = new SortedSet<string>();
+            for (int i = 0; i < n;i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                foreach (string element in input)
+                {
+                    elements.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
