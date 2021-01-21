fe6c64e296c0c548c45a597e83a7cf67378eef4e
BozhidarN7 <bojonemski@gmail.com>
2021-01-21 19:18:32 +0200

    Added Sets and Dictionaries problem

---
+++ C#Advanced/03.CSharp-Advanced-Sets-and-Dictionaries-Advanced-Exercise/5.CountSymbols/CountSymbols.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5.CountSymbols
+{
+    class CountSymbols
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            SortedDictionary<char, int> symbols = new SortedDictionary<char, int>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!symbols.ContainsKey(str[i]))
+                {
+                    symbols.Add(str[i], 0);
+                }
+                symbols[str[i]]++;
+            }
+            foreach (var pair in symbols)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value} time/s");
+            }
+        }
+    }
+}
