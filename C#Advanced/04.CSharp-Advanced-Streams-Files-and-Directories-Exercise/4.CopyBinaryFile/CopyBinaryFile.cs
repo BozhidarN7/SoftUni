5b7949a86fea3981f08327127538e6cd7c2e4135
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-25 12:41:47 +0200

    Added Files and Directories problem

---
+++ C#Advanced/04.CSharp-Advanced-Streams-Files-and-Directories-Exercise/4.CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace _4.CopyBinaryFile
+{
+    class CopyBinaryFile
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream reader = new FileStream(@"../../../../copyMe.png", FileMode.Open))
+            {
+                byte[] buffer = new byte[4096];
+                int bytesRead = 0;
+                using (FileStream writer = new FileStream(@"../../../../output.png", FileMode.Create, FileAccess.Write))
+                {
+                    while ((bytesRead = reader.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        writer.Write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        }
+    }
+}
