8f4c587d072bf777770b0bb0e5de0caa1632b28b
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-25 10:20:17 +0200

    Added Files and Directories problem

---
+++ C#Advanced/04.CSharp-Advanced-Streams-Files-and-Directories-Exercise/1.EvenLines/EvenLines.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _1.EvenLines
+{
+    class EvenLines
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader(@"../../../../text.txt"))
+            {
+                int row = 0;
+                string line = reader.ReadLine();
+                using (StreamWriter writer = new StreamWriter(@"../../../../output.txt"))
+                {
+                    while (line != null)
+                    {
+                        if ((row & 1) == 0)
+                        {
+                            line = Regex.Replace(line, @"[-,.!?]+", "@");
+                            writer.WriteLine(string.Join(" ",line.Split().Reverse()));
+                        }
+                        row++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+
+        }
+    }
+}
