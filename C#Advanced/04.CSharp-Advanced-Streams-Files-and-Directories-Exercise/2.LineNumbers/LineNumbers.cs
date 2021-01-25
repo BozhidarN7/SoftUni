b5c113efa651aa043777da7f5f2a3fabf5215802
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-25 10:51:34 +0200

    Added Files and Directories problem

---
+++ C#Advanced/04.CSharp-Advanced-Streams-Files-and-Directories-Exercise/2.LineNumbers/LineNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _2.LineNumbers
+{
+    class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"../../../../text.txt");
+            using (StreamWriter writer = File.AppendText(@"../../../../output.txt"))
+            {
+                int lineNumber = 1;
+                foreach (string line in lines)
+                {
+                    int punctuationMarksCount = Regex.Matches(line, @"[^\w\s]").Count;
+                    int letters = Regex.Matches(line, @"[a-zA-Z]").Count;
+                    writer.WriteLine($"Line {lineNumber}: {line}({letters})({punctuationMarksCount})");
+                    lineNumber++;
+                }
+            }
+        }
+    }
+}
