a4c5be38fe9e3f4e62646f672ee177e599bffbdf
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-25 07:45:18 +0200

    Added Files and Directories problem

---
+++ C#Advanced/04.CSharp-Advanced-Streams-Files-and-Directories-Lab/1.OddLines/OddLines.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace _1.OddLines
+{
+    class OddLines
+    {
+        static void Main(string[] args)
+        {
+            string directory = Environment.CurrentDirectory;
+            Console.WriteLine(directory);
+            using (StreamReader reader = new StreamReader(@"../../../../Resources/01.OddLines/input.txt")) // Use your path here
+            {
+                string line = reader.ReadLine();
+                int row = 0;
+                while (line != null)
+                {
+                    if (row % 2 == 1)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    row++;
+                    line = reader.ReadLine();
+                }
+            }
+
+        }
+    }
+}
