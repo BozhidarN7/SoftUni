46cee5f06fdf4b056af7ce556900fa092880b504
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-25 09:54:35 +0200

    Added Files and Directories problem

---
+++ C#Advanced/04.CSharp-Advanced-Streams-Files-and-Directories-Lab/6.FolderSize/FolderSize.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace _6.FolderSize
+{
+    class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles(@"../../../../Resources/06.FolderSize/TestFolder");
+            double sum = 0;
+            foreach(string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                sum += fileInfo.Length;
+            }
+
+            sum = sum / 1024 / 1024;
+            File.WriteAllText(@"../../../../Resources/06.FolderSize/TestFolder/output.txt", sum.ToString());
+        }
+    }
+}
