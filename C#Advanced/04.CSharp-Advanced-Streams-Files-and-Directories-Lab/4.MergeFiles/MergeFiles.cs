f1589b8ec7836812462194981ef6763fbe897188
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-25 09:06:38 +0200

    Added Files and Directories problem

---
+++ C#Advanced/04.CSharp-Advanced-Streams-Files-and-Directories-Lab/4.MergeFiles/MergeFiles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace _4.MergeFiles
+{
+    class MergeFiles
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader readerOne = new StreamReader(@"../../../../Resources/04.MergeFiles/FileOne.txt"))
+            {
+                using (StreamReader readerTwo = new StreamReader(@"../../../../Resources/04.MergeFiles/FileTwo.txt"))
+                {
+                    string lineOne = readerOne.ReadLine();
+                    string lineTwo = readerTwo.ReadLine();
+                    using (StreamWriter writer = new StreamWriter(@"../../../../Resources/04.MergeFiles/output.txt"))
+                    {
+                        while (lineOne != null && lineTwo != null)
+                        {
+                            writer.WriteLine(lineOne);
+                            writer.WriteLine(lineTwo);
+
+                            lineOne = readerOne.ReadLine();
+                            lineTwo = readerTwo.ReadLine();
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
