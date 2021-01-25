f03521b6d10abfde542e36d973f6ff7136c97076
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-25 12:20:21 +0200

    Added Files and Directories problem

---
+++ C#Advanced/04.CSharp-Advanced-Streams-Files-and-Directories-Exercise/3.WordCount/WordCount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _3.WordCount
+{
+    class WordCount
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> wordsOcc = new Dictionary<string, int>();
+            string[] lines = File.ReadAllLines(@"../../../../words.txt");
+            string text = File.ReadAllText(@"../../../../text.txt").ToLower();
+            foreach(string line in lines)
+            {
+                int occoccurrences = Regex.Matches(text, $@"\b({line})\b").Count;
+                if (!wordsOcc.ContainsKey(line))
+                {
+                    wordsOcc.Add(line, 0);
+                }
+                wordsOcc[line] += occoccurrences;
+            }
+            string actualResult = string.Empty;
+            foreach((string word,int occ) in wordsOcc.OrderByDescending(x => x.Value))
+            {
+                actualResult += $"{word} - {occ}{Environment.NewLine}";
+            }
+            File.WriteAllText(@"../../../../actualResult.txt", actualResult);
+
+            string expectedResult = File.ReadAllText(@"../../../../expectedResult.txt");
+
+            if (actualResult == expectedResult)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
