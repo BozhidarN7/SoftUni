69cd58a7eb6e590afe51110268fcbdbbd339a8e1
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-25 20:44:21 +0200

    Added Files and Directories problem

---
+++ C#Advanced/04.CSharp-Advanced-Streams-Files-and-Directories-Exercise/6.ZipAndExtract/ZipAndExtract.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace _6.ZipAndExtract
+{
+    class ZipAndExtract
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"../../../../ZippedFiles");
+            Directory.CreateDirectory(@"../../../../ExtractedFiles");
+            File.Move(@"../../../../copyMe.png", @"../../../../ZippedFiles/copyMe.png");
+            ZipFile.CreateFromDirectory(@"../../../../ZippedFiles", @"../../../../Zipped.zip");
+            ZipFile.ExtractToDirectory(@"../../../../Zipped.zip", @"../../../../ExtractedFiles");
+        }
+    }
+}
