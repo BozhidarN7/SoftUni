aeaf12c883e6bb74584eb4656fdef7a7e1323e4d
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-25 08:53:42 +0200

    Added Files and Directories problem

---
+++ C#Advanced/04.CSharp-Advanced-Streams-Files-and-Directories-Lab/3.WordCount/WordCount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _3.WordCount
+{
+    class WordCount
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> wordsOcc = new Dictionary<string, int>();
+            string text = File.ReadAllText(@"../../../../Resources/03.WordCount/text.txt").ToLower();
+            using (StreamReader reader = new StreamReader(@"../../../../Resources/03.WordCount/words.txt"))
+            {
+                string[] words = reader.ReadLine().Split();
+                foreach (string word in words)
+                {
+                    MatchCollection matches = Regex.Matches(text, @$"\b({word})\b");
+                    int count = matches.Count;
+                    if (!wordsOcc.ContainsKey(word))
+                    {
+                        wordsOcc.Add(word, 0);
+                    }
+                    wordsOcc[word] = count;
+
+                }
+            }
+            using (StreamWriter writer = new StreamWriter(@"../../../../Resources/03.WordCount/output.txt"))
+            {
+                foreach (var pair in wordsOcc.OrderByDescending(x => x.Value))
+                {
+                    writer.WriteLine($"{pair.Key} - {pair.Value}");
+                }
+            }
+
+        }
+    }
+}
