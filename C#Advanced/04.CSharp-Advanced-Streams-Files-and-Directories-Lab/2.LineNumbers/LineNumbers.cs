fff55b4663d4dd332379e981583beb8022dce08d
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-25 07:59:18 +0200

    Added Files and Directories problem

---
+++ C#Advanced/04.CSharp-Advanced-Streams-Files-and-Directories-Lab/2.LineNumbers/LineNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace _2.LineNumbers
+{
+    class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter writer = new StreamWriter(@"../../../../Resources/02.LineNumbers/output.txt")) // use your path
+            {
+                int lineNumber = 1;
+                using (StreamReader reader = new StreamReader(@"../../../../Resources/02.LineNumbers/input.txt")) // use your path
+                {
+                    string line = reader.ReadLine();
+                    while(line != null)
+                    {
+                        writer.WriteLine($"{lineNumber}. {line}");
+                        line = reader.ReadLine();
+                        lineNumber++;
+                    }
+                }
+            }
+        }
+    }
+}
