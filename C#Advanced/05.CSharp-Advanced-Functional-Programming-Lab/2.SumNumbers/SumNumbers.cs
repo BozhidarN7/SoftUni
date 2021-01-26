ca1f7b7a04b09f805a744f9a304b35bfe0d09621
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-26 18:19:27 +0200

    Added Functional programming problem

---
+++ C#Advanced/05.CSharp-Advanced-Functional-Programming-Lab/2.SumNumbers/SumNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.SumNumbers
+{
+    class SumNumbers
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+            Console.WriteLine(numbers.Count);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
