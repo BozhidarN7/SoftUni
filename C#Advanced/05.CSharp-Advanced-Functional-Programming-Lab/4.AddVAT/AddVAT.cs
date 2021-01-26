11873bcdae5d8d43e517150ca91cca5ed556be99
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-26 18:41:24 +0200

    Added Functional programming problem

---
+++ C#Advanced/05.CSharp-Advanced-Functional-Programming-Lab/4.AddVAT/AddVAT.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.AddVAT
+{
+    class AddVAT
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).Select(x => x + x * 0.2).ToList()
+             .ForEach(x => Console.WriteLine($"{x:f2}"));
+        }
+    }
+}
