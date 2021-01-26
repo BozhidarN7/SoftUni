1cec28a76042e5ceb4ed2d7a62fbcdfc327cc679
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-26 18:15:25 +0200

    Added Functional programming problem

---
+++ C#Advanced/05.CSharp-Advanced-Functional-Programming-Lab/1.SortEvenNumbers/SortEvenNumbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.SortEvenNumbers
+{
+    class SortEvenNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ", Console.ReadLine().Split(", ").Select(int.Parse).Where(x => x % 2 == 0).OrderBy(x => x).ToList()));
+        }
+    }
+}
