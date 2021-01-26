bf58cedefa956c2149a0922bc81ef61d6be0a9f3
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-26 18:30:25 +0200

    Added Functional programming problem

---
+++ C#Advanced/05.CSharp-Advanced-Functional-Programming-Lab/3.CountUppercaseWords/CountUppercaseWords.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.CountUppercaseWords
+{
+    class CountUppercaseWords
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> IsWordStartingWithCapitalLetter = (word) => word[0] >= 'A' && word[0] <= 'Z';
+
+             Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Where(x => IsWordStartingWithCapitalLetter(x)).ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
