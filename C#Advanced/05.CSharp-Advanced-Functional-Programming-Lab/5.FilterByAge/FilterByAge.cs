cefc8e9b45f3d591b89bc06b4b7aaf03d12c8609
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-27 08:55:05 +0200

    Added Functional programming problem

---
+++ C#Advanced/05.CSharp-Advanced-Functional-Programming-Lab/5.FilterByAge/FilterByAge.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5.FilterByAge
+{
+    class FilterByAge
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Person[] people = new Person[n];
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                people[i] = new Person();
+                people[i].Name = input[0];
+                people[i].Age = int.Parse(input[1]);
+            }
+
+            string filter = Console.ReadLine();
+            int filterAge = int.Parse(Console.ReadLine());
+            string format = Console.ReadLine();
+
+            PrintPeople(people, GetAgeCondition(filter, filterAge), GetFormatter(format));
+        }
+
+        private static Func<Person, bool> GetAgeCondition(string filter, int filterAge)
+        {
+            switch (filter)
+            {
+                case "younger": return p => p.Age < filterAge;
+                case "older": return p => p.Age >= filterAge;
+                default:
+                    return null;
+            }
+        }
+
+        private static Func<Person, string> GetFormatter(string format)
+        {
+            switch (format)
+            {
+                case "name": return p => $"{p.Name}";
+                case "age": return p => $"{p.Age}";
+                case "name age": return p => $"{p.Name} - {p.Age}";
+                default:
+                    return null;
+            }
+        }
+        private static void PrintPeople(Person[] people, Func<Person, bool> condition, Func<Person, string> formatter)
+        {
+            foreach (Person person in people)
+            {
+                if (condition(person))
+                {
+                    Console.WriteLine(formatter(person));
+                }
+            }
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
