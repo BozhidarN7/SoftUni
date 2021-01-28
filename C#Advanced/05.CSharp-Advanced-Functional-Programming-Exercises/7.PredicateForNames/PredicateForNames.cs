5baf2e56e721a9fc5c7965f99575a06eb56ab315
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-28 15:47:07 +0200

    Added Functional programming problem

---
+++ C#Advanced/05.CSharp-Advanced-Functional-Programming-Exercises/7.PredicateForNames/PredicateForNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _7.PredicateForNames
+{
+    class PredicateForNames
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.ReadLine().Split().Where(x=> x.Length <= n).ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
