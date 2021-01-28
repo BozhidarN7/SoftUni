e4dcb741d5d75a5979e6a8ab2de490463ab17e31
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-28 14:53:50 +0200

    Added Functional programming problem

---
+++ C#Advanced/05.CSharp-Advanced-Functional-Programming-Exercises/5.AppliedArithmetics/AppliedArithmetics.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _5.AppliedArithmetics
+{
+    class AppliedArithmetics
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                Func<int[], int[]> function = GetFunction(command);
+                if (function != null)
+                {
+                    arr =function(arr);
+                }
+                else
+                {
+                    Console.WriteLine(string.Join(" ", arr));
+                }
+                command = Console.ReadLine();
+            }
+        }
+
+        private static Func<int[], int[]> GetFunction(string command)
+        {
+            switch (command)
+            {
+                case "add": return arr => arr.Select(x => x + 1).ToArray();
+                case "multiply": return arr => arr.Select(x => x * 2).ToArray();
+                case "subtract": return arr => arr.Select(x => x - 1).ToArray();
+            }
+            return null;
+        }
+    }
+}
