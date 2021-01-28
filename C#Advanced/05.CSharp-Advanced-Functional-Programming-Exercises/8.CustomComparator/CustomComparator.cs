9f1d4dea176374dad6a28a184f221ca570638f8c
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-28 18:11:02 +0200

    Added Functional programming problem

---
+++ C#Advanced/05.CSharp-Advanced-Functional-Programming-Exercises/8.CustomComparator/CustomComparator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _8.CustomComparator
+{
+    class CustomComparator
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Array.Sort(arr, (a, b) =>
+            {
+                if (a % 2 == 0 && b % 2 !=0) return -1;
+                if (b % 2 == 0 && a % 2 != 0) return 1;
+
+                return a.CompareTo(b);
+            });
+            Console.WriteLine(string.Join(" ", arr));
+
+        }
+    }
+}
