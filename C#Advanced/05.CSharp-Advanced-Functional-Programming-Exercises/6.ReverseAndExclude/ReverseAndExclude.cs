ecacb2e88064913a62f3ade0ca1483ee57406979
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-28 15:36:35 +0200

    Added Functional programming problem

---
+++ C#Advanced/05.CSharp-Advanced-Functional-Programming-Exercises/6.ReverseAndExclude/ReverseAndExclude.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6.ReverseAndExclude
+{
+    class ReverseAndExclude
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+
+            Predicate<int> isDivisable = number => number % n == 0;
+            Func<int[], int[]> reverseArray = arr =>
+            {
+                List<int> reversedArr = new List<int>();
+                for (int i = arr.Length - 1; i >= 0; i--)
+                {
+                    if (!isDivisable(arr[i]))
+                    {
+                        reversedArr.Add(arr[i]);
+                    }
+                }
+                return reversedArr.ToArray();
+            };
+            Console.WriteLine(string.Join(" ", reverseArray(arr)));
+        }
+    }
+}
