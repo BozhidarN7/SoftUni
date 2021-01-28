7629f49a3b6b02101af626e3ed1e6777e4be5097
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-28 11:08:30 +0200

    Added Functional programming problem

---
+++ C#Advanced/05.CSharp-Advanced-Functional-Programming-Exercises/2.KnightsOfHonor/KnightsOfHonor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.KnightsOfHonor
+{
+    class KnightsOfHonor
+    {
+        static void Main(string[] args)
+        {
+            Action<string> appendSirAndPrint = str =>
+            {
+                str = "Sir " + str;
+                Console.WriteLine(str);
+            };
+            Console.ReadLine().Split().ToList().ForEach(x =>
+            {
+                appendSirAndPrint(x);
+            });
+        }
+
+
+    }
+}
