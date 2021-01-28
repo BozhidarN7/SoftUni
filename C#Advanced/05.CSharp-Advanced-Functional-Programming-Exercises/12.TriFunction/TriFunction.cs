9e985459045f818de1b207d9fe94aa2b98e70b53
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-28 18:50:56 +0200

    Added Functional programming problem

---
+++ C#Advanced/05.CSharp-Advanced-Functional-Programming-Exercises/12.TriFunction/TriFunction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+namespace _12.TriFunction
+{
+    class TriFunction
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] name = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Iterate(name, FindSpecialWord(), n);
+
+        }
+
+        private static void Iterate(string[] name, Func<string, int, bool> findSpecialWord, int n)
+        {
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (findSpecialWord(name[i],n))
+                {
+                    Console.WriteLine(name[i]);
+                    return;
+                }
+            }
+        }
+
+        private static Func<string, int, bool> FindSpecialWord()
+        {
+            return (str, n) =>
+            {
+                int sum = str.Select(c => (int)c).Sum();
+                if (sum >= n) return true;
+                return false;
+            };
+        }
+    }
+}
