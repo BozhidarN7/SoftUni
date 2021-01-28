6bba6b3da5e11ee11078e188306acc50e2b2f4c3
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-28 10:23:05 +0200

    Added Functional programming problem

---
+++ C#Advanced/05.CSharp-Advanced-Functional-Programming-Exercises/1.ActionPoint/ActionPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.ActionPoint
+{
+    class ActionPoint
+    {
+        static void Main(string[] args)
+        {
+            Action<string> printer = str => Console.WriteLine(str);
+            Console.ReadLine().Split().ToList().ForEach(x => printer(x));
+        }
+    }
+}
