022389e27ae8c8c76a983bd0505ed46fb85a0256
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-28 11:49:44 +0200

    Added Functional programming problem

---
+++ C#Advanced/05.CSharp-Advanced-Functional-Programming-Exercises/3.CustomMinFunction/CustomMinFunction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.CustomMinFunction
+{
+    class CustomMinFunction
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> findMinNumber = arr =>
+           {
+               int min = int.MaxValue;
+               for (int i = 0; i < arr.Length; i++)
+               {
+                   if (min > arr[i])
+                   {
+                       min = arr[i];
+                   }
+               }
+               return min;
+           };
+            Console.WriteLine(findMinNumber(Console.ReadLine().Split().Select(int.Parse).ToArray()));
+        }
+
+
+    }
+}
