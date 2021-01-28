17e29cc8a6d21f06562684d382990f250de0ed89
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-28 18:25:53 +0200

    Added Functional programming problem

---
+++ C#Advanced/05.CSharp-Advanced-Functional-Programming-Exercises/9.ListOfPredicates/ListOfPredicates.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _9.ListOfPredicates
+{
+    class ListOfPredicates
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] dividers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Predicate<int> isDivisable = num =>
+            {
+                foreach (int divider in dividers)
+                {
+                    if (num % divider != 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            };
+            for (int i = 1; i <= n; i++)
+            {
+                if (isDivisable(i))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
