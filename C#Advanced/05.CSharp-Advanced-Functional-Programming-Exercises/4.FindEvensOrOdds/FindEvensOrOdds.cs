a18d8104ce438718581b87bc91ee2fcb759049d7
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-28 12:56:13 +0200

    Added Functional programming problem

---
+++ C#Advanced/05.CSharp-Advanced-Functional-Programming-Exercises/4.FindEvensOrOdds/FindEvensOrOdds.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.FindEvensOrOdds
+{
+    class FindEvensOrOdds
+    {
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int start = tokens[0];
+            int end = tokens[1];
+            string conditoin = Console.ReadLine();
+
+            PrintNumbers(start, end, GetConditoin(conditoin));
+
+        }
+
+        private static void PrintNumbers(int start, int end, Predicate<int> condition)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                if (condition(i))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+
+        private static Predicate<int> GetConditoin(string condition)
+        {
+            if (condition == "odd")
+            {
+                return x => (x & 1) == 1;
+            }
+            else
+            {
+                return x => (x & 1) == 0;
+            }
+        }
+    }
+}
