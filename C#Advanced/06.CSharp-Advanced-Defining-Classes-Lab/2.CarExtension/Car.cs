fc5f3ae83632ee20caffac5ee576e29fed8b9a93
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-30 07:39:23 +0200

    Added Definin classes problem

---
+++ C#Advanced/06.CSharp-Advanced-Defining-Classes-Lab/2.CarExtension/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            if (this.FuelQuantity - distance * this.FuelConsumption > 0)
+            {
+                this.FuelQuantity -= distance * this.FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:f2}L";
+        }
+    }
+}
---
+++ C#Advanced/06.CSharp-Advanced-Defining-Classes-Lab/2.CarExtension/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Model = "M3";
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
