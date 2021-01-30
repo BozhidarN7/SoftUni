c3839d7bfc69ae6eb9789773241e12917542dfa7
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-30 07:26:29 +0200

    Added Definin classes problem

---
+++ C#Advanced/06.CSharp-Advanced-Defining-Classes-Lab/1.Car/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        string make;
+        string model;
+        int year;
+
+        public int Year { get => year; set => year = value; }
+        public string Model { get => model; set => model = value; }
+        public string Make { get => make; set => make = value; }
+    }
+}
