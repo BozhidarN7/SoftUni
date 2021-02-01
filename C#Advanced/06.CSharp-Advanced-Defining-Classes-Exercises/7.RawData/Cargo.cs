aedac31785403db14768f575d07311cb81326480
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-01 12:58:14 +0200

    Added Defining classes problem

---
+++ C#Advanced/06.CSharp-Advanced-Defining-Classes-Exercises/7.RawData/Cargo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Cargo
+    {
+        public Cargo(int weight ,string type)
+        {
+            Weight = weight;
+            Type = type;
+        }
+        public int Weight { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ C#Advanced/06.CSharp-Advanced-Defining-Classes-Exercises/7.RawData/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                Engine engine = new Engine(int.Parse(tokens[1]), int.Parse(tokens[2]));
+                Cargo cargo = new Cargo(int.Parse(tokens[3]), tokens[4]);
+                List<Tire> tires = new List<Tire>();
+                for (int j = 5; j <= 12; j += 2)
+                {
+                    tires.Add(new Tire(double.Parse(tokens[j]), int.Parse(tokens[j + 1])));
+                }
+                cars.Add(new Car(tokens[0], engine, cargo, tires));
+            }
+            string command = Console.ReadLine();
+            if (command == "fragile")
+            {
+                cars.Where(x => x.Cargo.Type == "fragile" && x.Tires.Where(y => y.Pressure < 1).Count() != 0).ToList().ForEach(x => Console.WriteLine(x.Model));
+            }
+            if (command == "flamable")
+            {
+                cars.Where(x => x.Cargo.Type == "flamable" && x.Engine.Power > 250).ToList().ForEach(x => Console.WriteLine(x.Model));
+            }
+        }
+    }
+}
