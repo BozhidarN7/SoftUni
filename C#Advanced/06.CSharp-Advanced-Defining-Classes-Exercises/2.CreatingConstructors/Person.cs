6c8b885b5beeb8980e13e35dad560900aadf1340
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-01 10:03:39 +0200

    Added Defining classes problem

---
+++ C#Advanced/06.CSharp-Advanced-Defining-Classes-Exercises/2.CreatingConstructors/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+        public Person(int age) 
+        {
+            Name = "No name";
+            Age = age;
+        }
+        public Person(string name, int age) : this(age)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ C#Advanced/06.CSharp-Advanced-Defining-Classes-Exercises/2.CreatingConstructors/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person person1 = new Person("Ivan", 3);
+            Console.WriteLine(person1.Age);
+        }
+    }
+}
