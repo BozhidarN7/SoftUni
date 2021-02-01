0edc8d8d63f3fa63e941847444cae65b45a03e47
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-01 10:37:33 +0200

    Added Defining classes problem

---
+++ C#Advanced/06.CSharp-Advanced-Defining-Classes-Exercises/5.DateModifier/DateModifier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        public int DaysDifference { get; set; }
+        public void CalculateDifference(string dateOne, string dateTwo)
+        {
+            DateTime firstDate = ConvertToDateType(dateOne);
+            DateTime secondDate = ConvertToDateType(dateTwo);
+
+            DaysDifference = Math.Abs((firstDate - secondDate).Days);
+        }
+
+        private DateTime ConvertToDateType(string date)
+        {
+            string[] tokens = date.Split();
+            int year = int.Parse(tokens[0]);
+            int month = int.Parse(tokens[1].StartsWith("0") ? tokens[1].Substring(1) : tokens[1]);
+            int day = int.Parse(tokens[2].StartsWith("0") ? tokens[2].Substring(1) : tokens[2]);
+
+            return new DateTime(year, month, day);
+        }
+    }
+}
---
+++ C#Advanced/06.CSharp-Advanced-Defining-Classes-Exercises/5.DateModifier/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string dateOne = Console.ReadLine();
+            string dateTwo = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier();
+            dateModifier.CalculateDifference(dateOne, dateTwo);
+            Console.WriteLine(dateModifier.DaysDifference);
+        }
+    }
+}
