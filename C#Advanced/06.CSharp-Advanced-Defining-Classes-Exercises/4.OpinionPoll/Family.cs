b50263f5a6c1e720b82915c20844a5fcf6b8c2e6
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-01 10:22:47 +0200

    Added Defining classes problem

---
+++ C#Advanced/06.CSharp-Advanced-Defining-Classes-Exercises/4.OpinionPoll/Family.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> People { get; set; }
+
+        public Family()
+        {
+            People = new List<Person>();
+        }
+        public void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+        public Person GetOldetsMember()
+        {
+            return People.OrderByDescending(x => x.Age).First();
+        }
+        public List<Person> GetOlderThanThirty()
+        {
+            return People.Where(x => x.Age > 30).OrderBy(x => x.Name).ToList();
+        }
+    }
+}
---
+++ C#Advanced/06.CSharp-Advanced-Defining-Classes-Exercises/4.OpinionPoll/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                family.AddMember(new Person(name, age));
+            }
+
+            List<Person> olderThanThirty = family.GetOlderThanThirty();
+            olderThanThirty.ForEach(x => Console.WriteLine($"{x.Name} - {x.Age}"));
+        }
+    }
+}
