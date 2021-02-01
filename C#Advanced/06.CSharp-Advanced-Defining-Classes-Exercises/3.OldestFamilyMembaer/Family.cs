482ed9b88b0b869cec1a84a48ce3c6830916c195
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-01 10:16:19 +0200

    Added Defining classes problem

---
+++ C#Advanced/06.CSharp-Advanced-Defining-Classes-Exercises/3.OldestFamilyMembaer/Family.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> People { get; set; }
+
+        public Family()
+        {
+            People = new List<Person>();
+        }
+        public void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+        public Person GetOldetsMember()
+        {
+            return People.OrderByDescending(x => x.Age).First();
+        }
+    }
+}
---
+++ C#Advanced/06.CSharp-Advanced-Defining-Classes-Exercises/3.OldestFamilyMembaer/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                family.AddMember(new Person(name, age));
+            }
+
+            Person oldestMember = family.GetOldetsMember();
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+}
