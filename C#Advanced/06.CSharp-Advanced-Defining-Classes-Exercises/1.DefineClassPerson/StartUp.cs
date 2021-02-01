796172c67dec0b10d5b8dc574683edc40da69128
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-01 09:43:43 +0200

    Added Defining classes problem

---
+++ C#Advanced/06.CSharp-Advanced-Defining-Classes-Exercises/1.DefineClassPerson/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.Age = 20;
+            person1.Name = "Pesho";
+
+            Person person2 = new Person();
+            person2.Age = 20;
+            person2.Name = "Pesho";
+
+            Person person3 = new Person();
+            person3.Age = 20;
+            person3.Name = "Pesho";
+
+        }
+    }
+}
