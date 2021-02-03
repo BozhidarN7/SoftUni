726f2fbbeac334e086c73677295bae2b502a25b9
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-03 14:51:43 +0200

    Added custom linked list

---
+++ C#Advanced/07.CSharp-Advanced-Workshop-Lab/CustomDoublyLinkedList/CustomLinkedList.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDoublyLinkedList
+{
+    public class CustomLinkedList
+    {
+        private int count = 0;
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+
+        public int[] ToArray()
+        {
+            int index = 0;
+            int[] array = new int[count];
+            ForEachFromHead((node) =>
+            {
+                array[index++] = node.Value;
+            });
+            return array;
+        }
+        public void ForEachFromHead(Action<Node> action)
+        {
+            Node currentNode = Head;
+            while (currentNode != null)
+            {
+                action(currentNode);
+                currentNode = currentNode.Next;
+            }
+        }
+        public void ForEachFromTail(Action<Node> action)
+        {
+            Node currentNode = Tail;
+            while (currentNode != null)
+            {
+                action(currentNode);
+                currentNode = currentNode.Previous;
+            }
+        }
+
+        public void AddHead(Node node)
+        {
+            count++;
+            if (Head is null)
+            {
+                Head = node;
+                Tail = node;
+                return;
+            }
+
+            node.Next = Head;
+            Head.Previous = node;
+            Head = node;
+
+        }
+        public void AddLast(Node node)
+        {
+            count++;
+            if (Tail is null)
+            {
+                Head = node;
+                Tail = node;
+                return;
+            }
+            node.Previous = Tail;
+            Tail.Next = node;
+            Tail = node;
+
+        }
+        public Node RemoveHead()
+        {
+            if (Head == null)
+            {
+                return null;
+            }
+            count--;
+            Node nodeToReturn = Head;
+            if (Head.Next != null)
+            {
+                Head = Head.Next;
+                Head.Previous = null;
+            }
+            else
+            {
+                Head = null;
+                Tail = null;
+            }
+            return nodeToReturn;
+
+        }
+        public Node RemoveTail()
+        {
+            if (Tail == null)
+            {
+                return null;
+            }
+            count--;
+            Node nodeToReturn = Tail;
+            if (Tail.Previous != null)
+            {
+                Tail = Tail.Previous;
+                Tail.Next = null;
+            }
+            else
+            {
+                Tail = null;
+                Head = null;
+            }
+            return nodeToReturn;
+        }
+    }
+
+    public class Node
+    {
+        public Node(int value)
+        {
+            Value = value;
+        }
+        public int Value { get; set; }
+        public Node Next { get; set; }
+        public Node Previous { get; set; }
+    }
+}
---
+++ C#Advanced/07.CSharp-Advanced-Workshop-Lab/CustomDoublyLinkedList/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CustomDoublyLinkedList
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomLinkedList linkedList = new CustomLinkedList();
+
+            for (int i = 0; i < 5; i++)
+            {
+                linkedList.AddLast(new Node(i + 1));
+            }
+            linkedList.ForEachFromTail(node => Console.WriteLine(node.Value));
+
+            Node node = linkedList.RemoveTail();
+            Console.WriteLine(node.Value);
+            linkedList.ForEachFromHead((node) => Console.WriteLine(node.Value));
+            //node = linkedList.RemoveTail();
+            int[] array = linkedList.ToArray();
+            Console.WriteLine(string.Join(" ", array));
+
+        }
+    }
+}
