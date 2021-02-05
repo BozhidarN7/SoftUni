2d7ee680604212dbfb0e9467ef1fe91127ce8741
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-05 09:44:36 +0200

    Implemented Custom Stack

---
+++ C#Advanced/07.CSharp-Advanced-Workshop-Exercise/CustomStack/CustomStack.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class CustomStack
+    {
+        private const int initialCapacity = 4;
+        private int[] array;
+
+        public CustomStack()
+        {
+            array = new int[initialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public void Push(int element)
+        {
+            if (Count == array.Length)
+            {
+                Resize();
+            }
+
+            array[Count] = element;
+            Count++;
+        }
+
+        public int Pop()
+        {
+            Validate();
+
+            int element = array[Count - 1];
+            array[Count - 1] = default;
+            Count--;
+            if (Count == array.Length / 4)
+            {
+                Shrink();
+            }
+            return element;
+        }
+        public int Peek()
+        {
+            Validate();
+
+            return array[Count - 1];
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                action(array[i]);
+            }
+        }
+        private void Validate()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty!");
+            }
+        }
+
+
+        private void Shrink()
+        {
+            int[] copy = new int[array.Length / 2];
+            Array.Copy(array, copy, Count);
+            array = copy;
+        }
+
+        private void Resize()
+        {
+            int[] copy = new int[array.Length * 2];
+            Array.Copy(array, copy, Count);
+            array = copy;
+        }
+    }
+}
---
+++ C#Advanced/07.CSharp-Advanced-Workshop-Exercise/CustomStack/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomStack
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomStack stack = new CustomStack();
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            stack.Push(6);
+
+            stack.ForEach(e => Console.WriteLine(e));
+        }
+    }
+}
