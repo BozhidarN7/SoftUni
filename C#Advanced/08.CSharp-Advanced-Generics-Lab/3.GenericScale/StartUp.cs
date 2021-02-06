4a06466c1b34bb6565f3e4ce2cdafe41ebdbfe26
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-06 18:01:56 +0200

    Added Generics problem

---
+++ C#Advanced/08.CSharp-Advanced-Generics-Lab/3.GenericScale/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> eq = new EqualityScale<int>(5, 6);
+            Console.WriteLine(eq.AreEqual());
+        }
+    }
+}
