0ff92b7cf48d697dad83a3f1e4bd0e644b7c8e42
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-06 17:50:17 +0200

    Added Generics problem

---
+++ C#Advanced/08.CSharp-Advanced-Generics-Lab/1.BoxOfT/BoxOfT.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class BoxOfT<T>
+    {
+        private Stack<T> stack;
+
+        public BoxOfT()
+        {
+            stack = new Stack<T>();
+        }
+        public int Count { get; private set; }
+
+        public void Add(T element)
+        {
+            stack.Push(element);
+            Count++;
+        }
+        public T Remove()
+        {
+            Count--;
+            return stack.Pop();
+
+        }
+    }
+}
