d9c025f13ceab3769b5d1a5de506b84ae1856544
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-08 17:44:33 +0200

    Added Generics problem

---
+++ C#Advanced/08.CSharp-Advanced-Generics-Exercises/1.GenericBoxOfString/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1.GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private T value;
+
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {value}";
+        }
+    }
+}
