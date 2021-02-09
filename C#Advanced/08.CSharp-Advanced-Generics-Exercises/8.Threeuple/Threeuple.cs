2cca618f1a5db134d7c652049751a0ee8d278608
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-09 07:18:21 +0200

    Added Generics problem

---
+++ C#Advanced/08.CSharp-Advanced-Generics-Exercises/8.Threeuple/Threeuple.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8.Threeuple
+{
+    class Threeuple<TFirst, TSecond,TThird>
+    {
+        private TFirst item1;
+        private TSecond item2;
+        private TThird item3;
+
+        public Threeuple(TFirst item1, TSecond item2,TThird item3)
+        {
+            Item1 = item1;
+            Item2 = item2;
+            Item3 = item3;
+        }
+        public TFirst Item1 { get; set; }
+        public TSecond Item2 { get; set; }
+        public TThird Item3 { get; set; }
+        public override string ToString()
+        {
+            return $"{Item1} -> {Item2} -> {Item3}";
+        }
+    }
+}
