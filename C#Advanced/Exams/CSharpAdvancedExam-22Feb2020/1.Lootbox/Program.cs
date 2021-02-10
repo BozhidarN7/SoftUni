796a2eee8cc1200092f965eceb754433b05c2d09
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-10 11:49:57 +0200

    Added first problem from the exam on 22 Feb 2020

---
+++ C#Advanced/Exams/CSharpAdvancedExam-22Feb2020/1.Lootbox/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.Lootbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> firstBox = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Stack<int> secondBox = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+            List<int> claimedItems = new List<int>();
+
+            while (firstBox.Count != 0 && secondBox.Count != 0)
+            {
+                int firstItem = firstBox.Peek();
+                int secondItem = secondBox.Peek();
+
+                if ((firstItem + secondItem) % 2 == 0)
+                {
+                    claimedItems.Add(firstItem + secondItem);
+                    firstBox.Dequeue();
+                    secondBox.Pop();
+                }
+                else
+                {
+                    firstBox.Enqueue(secondBox.Pop());
+                }
+            }
+
+            if (firstBox.Count == 0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            if (secondBox.Count == 0)
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+
+            if (claimedItems.Sum() >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {claimedItems.Sum()}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {claimedItems.Sum()}");
+            }
+        }
+    }
+}
