e973527f155d1eff8ca32f0505f67b7370b720d9
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-10 10:12:56 +0200

    Added Iterators and Comparators problem

---
+++ C#Advanced/09.CSharp-Advanced-Iterators-and-Comparators-Lab/1.Library/Library.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private readonly List<Book> books;
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            //return new LibraryIterator(books);
+            for (int i = 0; i < books.Count; i++)
+            {
+                yield return books[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ C#Advanced/09.CSharp-Advanced-Iterators-and-Comparators-Lab/1.Library/LibraryIterator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class LibraryIterator : IEnumerator<Book>
+    {
+        private List<Book> books;
+
+        private int currentIndex;
+        public LibraryIterator(List<Book> books)
+        {
+            this.Reset();
+            this.books = books;
+        }
+
+        public Book Current => books[currentIndex];
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+
+        }
+
+        public bool MoveNext()
+        {
+            currentIndex++;
+            return currentIndex < books.Count;
+        }
+
+        public void Reset()
+        {
+            currentIndex = -1;
+        }
+    }
+}
