2d76bde326df798f456400e73ef85ec4fa62fdbd
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-10 14:49:41 +0200

    Added the last problem from the exam on 22 Feb 2020

---
+++ C#Advanced/Exams/CSharpAdvancedExam-22Feb2020/Guild/Guild.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Guild
+{
+    public class Guild
+    {
+        private readonly List<Player> roster;
+
+        public Guild(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            roster = new List<Player>();
+        }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count => roster.Count;
+        public void AddPlayer(Player player)
+        {
+            if (roster.Count < Capacity)
+            {
+                roster.Add(player);
+            }
+        }
+        public bool RemovePlayer(string name)
+        {
+            Player player = roster.FirstOrDefault(x => x.Name == name);
+            if (player == null)
+            {
+                return false;
+            }
+            roster.Remove(player);
+            return true;
+        }
+        public void PromotePlayer(string name)
+        {
+            Player player = roster.FirstOrDefault(x => x.Name == name);
+            player.Rank = "Member";
+        }
+        public void DemotePlayer(string name)
+        {
+            Player player = roster.FirstOrDefault(x => x.Name == name);
+            player.Rank = "Trial";
+        }
+        public Player[] KickPlayersByClass(string classInput)
+        {
+            List<Player> playersToRemove = roster.Where(x => x.Class == classInput).ToList();
+            playersToRemove.ForEach(x => roster.Remove(x));
+            return playersToRemove.ToArray();
+        }
+        public string Report()
+        {
+            string result = $"Players in the guild: {Name}{Environment.NewLine}";
+            result += string.Join(Environment.NewLine, roster);
+            return result;
+        }
+    }
+}
