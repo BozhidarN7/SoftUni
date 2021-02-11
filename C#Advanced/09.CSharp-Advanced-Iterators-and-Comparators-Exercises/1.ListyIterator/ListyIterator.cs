e6ea24304e33519e7c85750ff432d4d7c0a6bf50
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-11 20:44:13 +0200

    Added Iterators and Comparators problem

---
+++ C#Advanced/09.CSharp-Advanced-Iterators-and-Comparators-Exercises/1.ListyIterator/ListyIterator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private readonly List<T> collection;
+        private int index;
+        public ListyIterator(params T[] data)
+        {
+            index = 0;
+            this.collection = new List<T>(data);
+        }
+
+        public bool Move()
+        {
+            if (index < collection.Count - 1)
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+        public bool HasNext()
+        {
+            if (index + 1 < collection.Count)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void Print()
+        {
+            if ( index >= collection.Count)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            Console.WriteLine(collection[index]);
+        }
+    }
+}
---
+++ C#Advanced/09.CSharp-Advanced-Iterators-and-Comparators-Exercises/1.ListyIterator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] tokens = input.Split().Skip(1).ToArray();
+            ListyIterator<string> list = new ListyIterator<string>(tokens);
+            while (input != "END")
+            {
+                input = Console.ReadLine();
+                if (input == "Print")
+                {
+                    try
+                    {
+                        list.Print();
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else if (input == "Move")
+                {
+                    Console.WriteLine(list.Move());
+                }
+                else if (input == "HasNext")
+                {
+                    Console.WriteLine(list.HasNext());
+                }
+            }
+        }
+    }
+}
