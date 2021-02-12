51b83399daaf9bbbdc37d1d355597262580fa46d
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-12 10:09:14 +0200

    Added Iterators and Comparators problem

---
+++ C#Advanced/09.CSharp-Advanced-Iterators-and-Comparators-Exercises/2.Collection/ListyIterator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2.Collection
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private readonly List<T> collection;
+        private int index;
+        public ListyIterator(params T[] data)
+        {
+            index = 0;
+            this.collection = new List<T>(data);
+        }
+
+        public bool Move()
+        {
+            if (index < collection.Count - 1)
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+        public bool HasNext()
+        {
+            if (index + 1 < collection.Count)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void Print()
+        {
+            if (index >= collection.Count || collection.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            Console.WriteLine(collection[index]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (collection.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            for (int i = 0; i < collection.Count; i++)
+            {
+                yield return collection[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ C#Advanced/09.CSharp-Advanced-Iterators-and-Comparators-Exercises/2.Collection/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] tokens = input.Split().Skip(1).ToArray();
+            ListyIterator<string> list = new ListyIterator<string>(tokens);
+            while (input != "END")
+            {
+                input = Console.ReadLine();
+                if (input == "Print")
+                {
+                    try
+                    {
+                        list.Print();
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else if (input == "PrintAll")
+                {
+                    try
+                    {
+                        Console.WriteLine(string.Join(" ", list));
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else if (input == "Move")
+                {
+                    Console.WriteLine(list.Move());
+                }
+                else if (input == "HasNext")
+                {
+                    Console.WriteLine(list.HasNext());
+                }
+            }
+        }
+    }
+}
