b05fdf8746c56d22991d8e8600dcb1197ec963a2
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-12 13:07:16 +0200

    Added Iterators and Comparators problem

---
+++ C#Advanced/09.CSharp-Advanced-Iterators-and-Comparators-Exercises/6.EqualityLogic/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _6.EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override int GetHashCode()
+        {
+            int nameHash = Name.GetHashCode();
+            int ageHash = Age.GetHashCode();
+
+            return nameHash + ageHash;
+        }
+        public override bool Equals(object obj)
+        {
+            return this.GetHashCode() == obj.GetHashCode();
+        }
+
+        public int CompareTo(Person other)
+        {
+            int result = Name.CompareTo(other.Name);
+            if (result == 0)
+            {
+                return Age.CompareTo(other.Age);
+            }
+            return result;
+        }
+    }
+}
---
+++ C#Advanced/09.CSharp-Advanced-Iterators-and-Comparators-Exercises/6.EqualityLogic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6.EqualityLogic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Person> hasSet = new HashSet<Person>();
+            SortedSet<Person> sortedSet = new SortedSet<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                Person person = new Person(tokens[0], int.Parse(tokens[1]));
+                hasSet.Add(person);
+                sortedSet.Add(person);
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hasSet.Count);
+        }
+    }
+}
