0b337bc18368857bb866fd94ea0377126d1987f3
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-12 10:10:01 +0200

    Added Iterators and Comparators problem

---
+++ C#Advanced/09.CSharp-Advanced-Iterators-and-Comparators-Exercises/3.Stack/CustomStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3.Stack
+{
+    public class CustomStack : IEnumerable<int>
+    {
+        private readonly List<int> elements;
+
+        public CustomStack()
+        {
+            this.elements = new List<int>();
+        }
+
+        public void Push(List<int> items)
+        {
+            foreach (int item in items)
+            {
+                elements.Add(item);
+            }
+        }
+
+        public void Pop()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+            elements.RemoveAt(elements.Count - 1);
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = elements.Count - 1; i >= 0; i--)
+            {
+                yield return elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ C#Advanced/09.CSharp-Advanced-Iterators-and-Comparators-Exercises/3.Stack/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            CustomStack stack = new CustomStack();
+            while (input  != "END")
+            {
+                string[] tokens = input.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                if (tokens[0] == "Push")
+                {
+                    stack.Push(tokens.Skip(1).Select(int.Parse).ToList());
+                }
+                else if (tokens[0] == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch(InvalidOperationException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                input = Console.ReadLine();
+            }
+
+            foreach(int item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (int item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
