32d3b62d5a3387644228e4df3a13c45b0c04e610
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-12 18:00:42 +0200

    Added the third problem from the exam on 16 December 2020

---
+++ C#Advanced/Exams/CSharpAdvancedRetakeExam-16December2020/3.Openning/Bakery.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        private readonly List<Employee> data;
+
+        public Bakery(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Employee>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count => data.Count;
+        public void Add(Employee employee)
+        {
+            if (data.Count < Capacity)
+            {
+                data.Add(employee);
+            }
+        }
+        public bool Remove(string name)
+        {
+            int removed =data.RemoveAll(x => x.Name == name);
+            if (removed > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Employee GetOldestEmployee()
+        {
+            return data.OrderByDescending(x => x.Age).First();
+        }
+        public Employee GetEmployee(string name)
+        {
+            return data.FirstOrDefault(x => x.Name == name);
+        }
+
+        public string Report() 
+        {
+            return $"Employees working at Bakery {Name}:{Environment.NewLine}" + string.Join(Environment.NewLine, data.Select(x => x.ToString()));
+        }
+
+    }
+}
