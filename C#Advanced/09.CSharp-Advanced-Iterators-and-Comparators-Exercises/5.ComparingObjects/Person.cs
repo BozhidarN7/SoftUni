d8279e31b8d4bc7882c04c8f10bd4392958f1253
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-12 10:50:13 +0200

    Added Iterators and Comparators problem

---
+++ C#Advanced/09.CSharp-Advanced-Iterators-and-Comparators-Exercises/5.ComparingObjects/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _5.ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            Name = name;
+            Age = age;
+            Town = town;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+
+        public int CompareTo( Person other)
+        {
+            if (Name.CompareTo(other.Name) != 0)
+            {
+                return Name.CompareTo(other.Name);
+            }
+            if (Age.CompareTo(other.Age) != 0)
+            {
+                return Age.CompareTo(other.Age);
+            }
+            return Town.CompareTo(other.Town);
+        }
+    }
+}
---
+++ C#Advanced/09.CSharp-Advanced-Iterators-and-Comparators-Exercises/5.ComparingObjects/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5.ComparingObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                string[] tokens = input.Split();
+                people.Add(new Person(tokens[0], int.Parse(tokens[1]), tokens[2]));
+                input = Console.ReadLine();
+            }
+            int index = int.Parse(Console.ReadLine()) - 1;
+            int same = 0;
+            for (int i = 0; i < people.Count; i++)
+            {
+                if (i != index && people[index].CompareTo(people[i]) == 0)
+                {
+                    same++;
+                }
+            }
+            if (same == 0)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                same += 1;
+                Console.WriteLine($"{same} {people.Count -same} {people.Count}");
+            }
+        }
+    }
+}
