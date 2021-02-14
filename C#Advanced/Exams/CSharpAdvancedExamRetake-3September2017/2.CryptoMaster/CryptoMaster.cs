a63c99678b91033370c2e6a39120293f59ef65ba
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-14 15:03:38 +0200

    Added the second problem from the exam on 3 September 2017

---
+++ C#Advanced/Exams/CSharpAdvancedExamRetake-3September2017/2.CryptoMaster/CryptoMaster.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.CryptoMaster
+{
+    class CryptoMaster
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int maxCount = 1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 1; j <= numbers.Length; j++)
+                {
+                    int currentMax = 1;
+                    int previous = i;
+                    int index = (i + j) % numbers.Length;
+
+                    while (numbers[i] != numbers[index])
+                    {
+                        if (numbers[index] > numbers[previous])
+                        {
+                            currentMax++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        previous = index;
+                        index = (index + j) % numbers.Length;
+                    }
+
+                    if ( maxCount < currentMax)
+                    {
+                        maxCount = currentMax;
+                    }
+                }
+            }
+            Console.WriteLine(maxCount);
+        }
+    }
+}
