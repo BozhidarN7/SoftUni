3242c3f61be5870629bae329c9623e3a370bbe5f
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-15 18:53:05 +0200

    Added Advanced Workshop Basic Algorithms problem

---
+++ C#Advanced/10.CSharp-Advanced-Workshop-Basic-Algorithms/1.RecursiveArraySum/RecursiveArraySum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.RecursiveArraySum
+{
+    class RecursiveArraySum
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(CalculateSum(numbers, numbers.Length - 1));
+        }
+
+        private static int CalculateSum(int[] numbers, int index)
+        {
+            if (index == 0)
+            {
+                return numbers[index];
+            }
+            return numbers[index] + CalculateSum(numbers,index - 1);
+        }
+    }
+}
