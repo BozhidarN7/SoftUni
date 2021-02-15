890c7e2d87165c55fe74360d038be303b492b14e
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-15 19:00:13 +0200

    Added Advanced Workshop Basic Algorithms problem

---
+++ C#Advanced/10.CSharp-Advanced-Workshop-Basic-Algorithms/2.RecursiveFactorial/RecursiveFactorial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _2.RecursiveFactorial
+{
+    class RecursiveFactorial
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CalculateFactorial(n));
+        }
+
+        private static int CalculateFactorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * CalculateFactorial(n - 1);
+        }
+    }
+}
