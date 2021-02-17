5cf0734e6b108a057306775dce16e81d5e93314c
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-17 09:21:07 +0200

    Added the first problem from the exam on 25 June 2017

---
+++ C#Advanced/Exams/CSharpAdvancedExam-25June2017/1.Regex/Regeh.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _1.Regeh
+{
+    class Regeh
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex regex = new Regex(@"\[[A-Z&a-z]+<([0-9]+)REGEH([0-9]+)>[A-Z&a-z]+\]");
+            MatchCollection matches = regex.Matches(input);
+            string result = "";
+            int index = 0;
+            foreach (Match match in matches)
+            {
+                int current = int.Parse(match.Groups[1].Value);
+                index += current;
+                index %= input.Length;
+                result += input[index];
+
+                current = int.Parse(match.Groups[2].Value);
+                index += current;
+                index %= input.Length;
+                result += input[index];
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
