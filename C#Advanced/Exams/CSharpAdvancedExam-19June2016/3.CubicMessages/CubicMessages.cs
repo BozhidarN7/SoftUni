d77e9d5a5721838a3588023d3cd62b285c545ae3
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-18 11:51:56 +0200

    Added the third problem from the exam on 19 June 2016

---
+++ C#Advanced/Exams/CSharpAdvancedExam-19June2016/3.CubicMessages/CubicMessages.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _3.CubicMessages
+{
+    class CubicMessages
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if(input == "Over!")
+                {
+                    break;
+                }
+
+                int length = int.Parse(Console.ReadLine());
+                Regex regex = new Regex($@"^(?<digits>\d+)(?<message>[A-Za-z]{{{length}}})(?<after>[^A-Za-z]*)$");
+
+                Match match = regex.Match(input);
+
+                if (!match.Success)
+                {
+                    continue;
+                }
+
+                string message = match.Groups["message"].Value;
+                string digitsBefore = match.Groups["digits"].Value;
+                string digitsAfter = string.Join("", match.Groups["after"].Value.Where(x => char.IsDigit(x)));
+
+                string decrypted = "";
+                for (int i = 0; i < digitsBefore.Length; i++)
+                {
+                    int digit = digitsBefore[i] - '0';
+                    if (digit >=0 && digit < message.Length)
+                    {
+                        decrypted += message[digit];
+                    }
+                    else
+                    {
+                        decrypted += " ";
+                    }
+                           
+                }
+
+                for (int i = 0; i < digitsAfter.Length; i++)
+                {
+                    int digit = digitsAfter[i] - '0';
+                    if (digit >= 0 && digit < message.Length)
+                    {
+                        decrypted += message[digit];
+                    }
+                    else
+                    {
+                        decrypted += " ";
+                    }
+
+                }
+
+                Console.WriteLine($"{message} == {decrypted}");
+            }
+        }
+    }
+}
