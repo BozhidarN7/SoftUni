a117bec4154f6200ee569002adbae3c000da4f33
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-20 14:42:05 +0200

    Added the first problem from the exam on 20 of February 2021

---
+++ C#Advanced/Exams/CSharpExam-20February2021/TheFightForGondor/TheFightForGondor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheFightForGondor
+{
+    class TheFightForGondor
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
