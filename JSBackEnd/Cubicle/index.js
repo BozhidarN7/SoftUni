2df36dbca2bbb25b8c59d2e995be5cf291eed788
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-23 17:29:14 +0200

    Added main routes

--- JSBackEnd/Cubicle/index.js
+++ JSBackEnd/Cubicle/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const config = require('./config/config');
+const expressConfig = require('./config/express');
+const routes = require('./routes');
 
 const app = new express();
+
+// requires('./config/express')(app);
+expressConfig(app);
+
+app.use(routes);
+
 app.listen(config.PORT, () => console.log(`Listening on port ${config.PORT}...`))
