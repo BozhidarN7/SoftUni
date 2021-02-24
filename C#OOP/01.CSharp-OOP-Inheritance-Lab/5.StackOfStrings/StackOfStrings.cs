22e2a72c8175bd677a3bb31b2837745ff32d8bea
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-24 07:06:34 +0200

    Added another problem from Inheritance

---
+++ C#OOP/01.CSharp-OOP-Inheritance-Lab/5.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return Count == 0 ? true : false;
+        }
+        public Stack<string> AddRange(IEnumerable<string> data)
+        {
+            foreach (string item in data)
+            {
+                this.Push(item);
+            }
+            return this;
+        }
+    }
+}
---
+++ C#OOP/01.CSharp-OOP-Inheritance-Lab/5.StackOfStrings/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            StackOfStrings cs = new StackOfStrings();
+            cs.Push("1");
+            cs.Push("2");
+            cs.Push("3");
+            cs.Push("4");
+            cs.AddRange(new string[] { "5", "6" });
+            Console.WriteLine(string.Join(" ", cs));
+        }
+    }
+}
