ed8c1f3cb354abe32492e7ee916c7390b78dd876
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-24 07:00:15 +0200

    Added another problem from Inheritance

---
+++ C#OOP/01.CSharp-OOP-Inheritance-Lab/4.RandomList/RandomList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random random = new Random();
+            if (this.Count == 0)
+            {
+                return default;
+            }
+
+            return this[random.Next(0, this.Count)];
+        }
+    }
+}
---
+++ C#OOP/01.CSharp-OOP-Inheritance-Lab/4.RandomList/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            RandomList rl = new RandomList();
+            rl.Add("1");
+            rl.Add("2");
+            rl.Add("3");
+            rl.Add("4");
+            Console.WriteLine(rl.RandomString());
+            Console.WriteLine(string.Join(" ", rl));
+        }
+    }
+}
