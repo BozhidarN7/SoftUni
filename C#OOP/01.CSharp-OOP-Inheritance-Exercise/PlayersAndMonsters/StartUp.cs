6024b8c2cf95c1239ce4cba42b176bf6bfe7466e
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-25 07:09:47 +0200

    Added another problem from the Inheritance exercises

---
+++ C#OOP/01.CSharp-OOP-Inheritance-Exercise/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Knight knight = new Knight("zombi200", 20);
+            System.Console.WriteLine(knight.GetType().Name);
+        }
+    }
+}
