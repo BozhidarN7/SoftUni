252aa8ef75bd4d60f5124b93f36969a44a2aab41
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-25 06:55:00 +0200

    Added another problem from the Inheritance exercises

---
+++ C#OOP/01.CSharp-OOP-Inheritance-Exercise/Zoo/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Bear bear = new Bear("Ivan");
+            Console.WriteLine(typeof(Bear));
+        }
+    }
+}
