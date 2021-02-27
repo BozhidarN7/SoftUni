d4ee2c28c0b466199581ed6d94e46abc412deb72
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-27 15:00:01 +0200

    Added the first problem from the Encapsulation lab

---
+++ C#OOP/02.CSharp-OOP-Encapsulation-Lab/1.SortPersonsByNameAndAge/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                people.Add(new Person(input[0], input[1], int.Parse(input[2])));
+
+            }
+            people.OrderBy(x => x.FirstName).ThenBy(x => x.Age).ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
