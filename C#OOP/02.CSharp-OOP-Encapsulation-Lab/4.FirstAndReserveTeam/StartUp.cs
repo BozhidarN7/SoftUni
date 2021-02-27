1fea4abd120b242d883c237e8c51171e321ce6bd
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-27 15:28:43 +0200

    Added the final problem from the Encapsulation lab

---
+++ C#OOP/02.CSharp-OOP-Encapsulation-Lab/4.FirstAndReserveTeam/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            Team team = new Team("SoftUni");
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0],
+                                        cmdArgs[1],
+                                        int.Parse(cmdArgs[2]),
+                                        decimal.Parse(cmdArgs[3]));
+
+                persons.Add(person);
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine(team.FirstTeam.Count);
+            Console.WriteLine(team.ReserveTeam.Count);
+        }
+    }
+}
