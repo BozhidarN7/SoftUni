7c9019a6972ab1d4772729de7ca4f8a1e6772780
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-28 09:38:33 +0200

    Added the first problem from the Encapsulation exercise

---
+++ C#OOP/02.CSharp-OOP-Encapsulation-Exercise/1.ClassBoxData/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Box
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+
+
+                Console.WriteLine($"Surface Area - {box.CalculateSurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.CalculateLateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.CalculateVolume():f2}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
