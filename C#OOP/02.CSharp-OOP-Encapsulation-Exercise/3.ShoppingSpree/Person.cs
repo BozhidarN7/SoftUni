f2164cfe851cbe1e85b72fd29a86daba19a4101b
Bozhidar Nemski <bojonemski@gmail.com>
2021-03-01 21:46:47 +0200

    Added another problem from the Encapsulation exercise

---
+++ C#OOP/02.CSharp-OOP-Encapsulation-Exercise/3.ShoppingSpree/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bag;
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            bag = new List<Product>();
+        }
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+        public decimal Money
+        {
+            get => money;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+        public IReadOnlyCollection<Product> Bag => bag.AsReadOnly();
+        public void AddProduct(Product product)
+        {
+            if (product.Cost > money)
+            {
+                throw new InvalidOperationException($"{Name} can't afford {product.Name}");
+            }
+            bag.Add(product);
+            Money -= product.Cost;
+        }
+    }
+}
---
+++ C#OOP/02.CSharp-OOP-Encapsulation-Exercise/3.ShoppingSpree/StartUp.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingSpree
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, Person> people = new Dictionary<string, Person>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] tokens = input[i].Split('=', StringSplitOptions.RemoveEmptyEntries);
+                try
+                {
+                    people.Add(tokens[0], new Person(tokens[0], decimal.Parse(tokens[1])));
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+            }
+
+            input = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, Product> products = new Dictionary<string, Product>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] tokens = input[i].Split('=', StringSplitOptions.RemoveEmptyEntries);
+                try
+                {
+                    products.Add(tokens[0], new Product(tokens[0], decimal.Parse(tokens[1])));
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+            }
+
+            string line = Console.ReadLine();
+            while (line != "END")
+            {
+                string[] tokens = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string personName = tokens[0];
+                string productName = tokens[1];
+                try
+                {
+                    people[personName].AddProduct(products[productName]);
+                    Console.WriteLine($"{personName} bought {productName}");
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                line = Console.ReadLine();
+            }
+            foreach ((string name, Person person) in people)
+            {
+                if (person.Bag.Count != 0)
+                {
+                    Console.WriteLine($"{name} - {string.Join(", ", person.Bag.Select(x => x.Name))}");
+                }
+                else
+                {
+                    Console.WriteLine($"{name} - Nothing bought");
+                }
+
+            }
+        }
+    }
+}
