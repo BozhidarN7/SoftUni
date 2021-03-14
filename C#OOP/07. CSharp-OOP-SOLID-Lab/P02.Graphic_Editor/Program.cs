bd8a94586f1bca175ede0935e2e3e8ad9795cb5e
Bozhidar Nemski <bojonemski@gmail.com>
2021-03-14 10:31:53 +0200

    Added another problem from the SOLID Lab

---
+++ C#OOP/07. CSharp-OOP-SOLID-Lab/P02.Graphic_Editor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P02.Graphic_Editor
+{
+    class Program
+    {
+        static void Main()
+        {
+            GraphicEditor ge = new GraphicEditor();
+
+            Circle circle = new Circle();
+            ge.DrawShape(circle);
+
+            Rectangle rectangle = new Rectangle();
+            ge.DrawShape(rectangle);
+
+            IShape squareShape = new Square();
+            ge.DrawShape(squareShape);
+        }
+    }
+}
