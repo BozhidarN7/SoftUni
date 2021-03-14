97a79a2523d015aa37478825f7e6064b76056550
Bozhidar Nemski <bojonemski@gmail.com>
2021-03-14 12:08:47 +0200

    Added another problem from the SOLID Lab

---
+++ C#OOP/07. CSharp-OOP-SOLID-Lab/P03.Detail_Printer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03.DetailPrinter
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Manager("Bozhidar",new List<string>(){"something" }),
+                new Employee("Ivan")
+            };
+            DetailsPrinter dp = new DetailsPrinter(employees);
+            dp.PrintDetails();
+        }
+    }
+}
