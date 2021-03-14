d447003c211f5efdc0c8311354c4088c903985b8
Bozhidar Nemski <bojonemski@gmail.com>
2021-03-14 10:23:41 +0200

    Added the first problem from the SOLID Lab

---
+++ C#OOP/07. CSharp-OOP-SOLID-Lab/P01.Stream_Progress/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        static void Main()
+        {
+            File file = new File("Programmer.txt", 50, 10);
+            StreamProgressInfo spr = new StreamProgressInfo(file);
+            Console.WriteLine(spr.CalculateCurrentPercent());
+
+            Music music = new Music("Something", "Something", 100, 10);
+            spr = new StreamProgressInfo(music);
+            Console.WriteLine($"{spr.CalculateCurrentPercent()}% sent so far");
+        }
+    }
+}
