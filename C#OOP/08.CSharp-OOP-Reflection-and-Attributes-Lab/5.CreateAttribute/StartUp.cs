42db33f566961f82b5aed4fc3bae21ac104c19a9
Bozhidar Nemski <bojonemski@gmail.com>
2021-03-17 17:13:29 +0200

    Added another problem from the Reflection lab

---
+++ C#OOP/08.CSharp-OOP-Reflection-and-Attributes-Lab/5.CreateAttribute/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Bozhidar")]
+    class StartUp
+    {
+        [Author("Bozhidar")]
+        static void Main(string[] args)
+        {
+        }
+    }
+}
