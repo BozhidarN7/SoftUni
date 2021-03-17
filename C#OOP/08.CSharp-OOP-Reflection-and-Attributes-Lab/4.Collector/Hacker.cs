68458ce8feb4992a60824c263ac61937b1f858da
Bozhidar Nemski <bojonemski@gmail.com>
2021-03-17 16:57:55 +0200

    Added another problem from the Reflectoin lab

---
+++ C#OOP/08.CSharp-OOP-Reflection-and-Attributes-Lab/4.Collector/Hacker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stealer
+{
+    public class Hacker
+    {
+        public string username = "securityGod82";
+        private string password = "mySuperSecretPasssw0rd";
+
+        public string Passsword
+        {
+            get => password;
+            set => password = value;
+        }
+
+        private int Id { get; set; }
+        public double BankAccountBalance { get; private set; }
+        public void DownloadAllBankAccountsInTheWorld()
+        {
+
+        }
+    }
+}
