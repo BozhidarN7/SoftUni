4dd8c3cb23461f275b71e926435a8c7a26da878a
Bozhidar Nemski <bojonemski@gmail.com>
2021-03-19 11:00:28 +0200

    Added the second problem from the Reflection exercise

---
+++ C#OOP/08.CSharp-OOP-Reflection-and-Attributes-Exercise/ValidationAttributes/Attributes/MyRangeAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes.Attributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private readonly int minValue;
+        private readonly int maxValue;
+        private readonly bool inclusive;
+        public MyRangeAttribute(int minValue, int maxValue, bool inclusive = false)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+            this.inclusive = inclusive;
+        }
+        public override bool IsValid(object obj)
+        {
+            int number = (int)obj;
+            if (inclusive)
+            {
+                return number >= minValue && number <= maxValue;
+            }
+            return number > minValue && number < maxValue;
+        }
+    }
+}
---
+++ C#OOP/08.CSharp-OOP-Reflection-and-Attributes-Exercise/ValidationAttributes/Validator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] properties = obj.GetType().GetProperties();
+            foreach(PropertyInfo property in properties)
+            {
+                MyValidationAttribute[] attributes = property.GetCustomAttributes().Cast<MyValidationAttribute>().ToArray();
+                object? value = property.GetValue(obj);
+                foreach(MyValidationAttribute attribute in attributes)
+                {
+                    bool isValid = attribute.IsValid(value);
+                    if (!isValid)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
