f7d02a3a21cc379954663e2257464d5ef44463a9
Bozhidar Nemski <bojonemski@gmail.com>
2021-03-28 22:16:23 +0300

    Implemented Facade Design Pattern

---
+++ C#OOP/11.CSharp-OOP-Design-Patterns-Lab/2.Facade/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2.Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new CarBuilderFacade()
+                .Info
+
+                .Built
+                .InCity("Leipzig")
+                .AtAddress("Some address 245")
+            .Build();
+
+            Console.WriteLine(car);
+        }
+    }
+}
