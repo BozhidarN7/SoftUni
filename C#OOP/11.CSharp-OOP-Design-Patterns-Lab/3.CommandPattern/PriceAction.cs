93227c941ecbf1ebc6add8960f6367186de5a647
Bozhidar Nemski <bojonemski@gmail.com>
2021-03-29 08:56:34 +0300

    Added Command Design Pattern

---
+++ C#OOP/11.CSharp-OOP-Design-Patterns-Lab/3.CommandPattern/PriceAction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3.CommandPattern
+{
+    public enum PriceAction
+    {
+        Increase,
+        Decrease
+    }
+}
---
+++ C#OOP/11.CSharp-OOP-Design-Patterns-Lab/3.CommandPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _3.CommandPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ModifyPrice modifyPrice = new ModifyPrice();
+            Product product = new Product("Phone", 500);
+
+            Execute(modifyPrice, new ProductCommand(product, PriceAction.Increase, 100));
+            Execute(modifyPrice, new ProductCommand(product, PriceAction.Increase, 50));
+            Execute(modifyPrice, new ProductCommand(product, PriceAction.Decrease, 25));
+            Console.WriteLine(product);
+        }
+
+        private static void Execute(ModifyPrice modifyPrice, ProductCommand productCommand)
+        {
+            modifyPrice.SetCommand(productCommand);
+            modifyPrice.Invoke();
+        }
+    }
+}
