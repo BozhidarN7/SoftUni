7abda19f66d74c193de7293f1a54a8736d0c484d
Bozhidar Nemski <bojonemski@gmail.com>
2021-03-30 10:14:52 +0300

    Added Template Pattern

---
+++ C#OOP/11.CSharp-OOP-Design-Patterns-Exercise/3.TemplatePattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3.TemplatePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sourdough sourdoght = new Sourdough();
+            sourdoght.Make();
+
+            TwelveGrain twelveGrain = new TwelveGrain();
+            twelveGrain.Make();
+
+            WholeWheat wholeWheat = new WholeWheat();
+            wholeWheat.Make();
+        }
+    }
+}
