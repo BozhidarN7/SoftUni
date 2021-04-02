5d10f5a11bc16571fcb8880c22d11f0445b60b69
Bozhidar Nemski <bojonemski@gmail.com>
2021-04-02 12:04:38 +0300

    Added login functionality

--- JSBackEnd/Exams/SoftUniWiki/controllers/authController.js
+++ JSBackEnd/Exams/SoftUniWiki/controllers/authController.js
@@ -1,16 +1,47 @@
 const express = require('express');
 
+const authService = require('../services/authService')
+
 const router = express.Router();
 
 
 router.route('/login')
     .get((req, res) => {
         res.render('login');
+    })
+    .post(async(req, res) => {
+        try {
+            const token = await authService.login(req.body);
+            res.redirect('/');
+        } catch (error) {
+            console.log(error);
+            res.render('login', { error });
+        }
     });
 
 router.route('/register')
     .get((req, res) => {
+
         res.render('register');
     })
+    .post(async(req, res) => {
+        if (req.body.password !== req.body.repeatPassword) {
+            res.render(('register'), { error: { message: 'Both passwords should be the same' } });
+            return;
+        }
+        if (await authService.duplicateUserNameFound(req.body.username)) {
+            res.render(('register'), { error: { message: 'Already have user with that username' } });
+            return;
+        }
+
+        try {
+            await authService.registerUser(req.body);
+            res.redirect('/');
+        } catch (err) {
+            // console.log(err);
+            res.render('register');
+        }
+    })
+
 
 module.exports = router;
--- JSBackEnd/Exams/SoftUniWiki/services/authService.js
+++ JSBackEnd/Exams/SoftUniWiki/services/authService.js
@@ -1,12 +1,33 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const config = require('../config/config');
 const User = require('../models/user');
+
+const signToken = id => {
+    return jwt.signToken({ id }, config.JWT_SECRET, { expiresIn: `${config.JWT_EXPIRES_IN}d` });
+}
 
 exports.registerUser = async({ username, password }) => {
     const salt = await bcrypt.genSalt(config.SALT_ROUNDS);
     const hash = await bcrypt.hash(password, salt);
     return User.create({ username, password: hash });
 
-}
+};
+
+exports.duplicateUserNameFound = async(username) => {
+    const user = await User.findOne({ username });
+    if (user) {
+        return 1;
+    }
+    return 0;
+};
+
+exports.login = async({ username, password }) => {
+    const user = await User.findOne({ username });
+    if (!user) throw { message: 'Incorrect username or password' };
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw { message: 'Incorrect username or password' };
+};
