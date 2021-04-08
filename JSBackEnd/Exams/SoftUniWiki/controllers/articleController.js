41b79d76f101441bb0c9ebf719564d2189b4b221
Bozhidar Nemski <bojonemski@gmail.com>
2021-04-08 18:46:39 +0300

    Added edit article functionality

--- JSBackEnd/Exams/SoftUniWiki/controllers/articleController.js
+++ JSBackEnd/Exams/SoftUniWiki/controllers/articleController.js
@@ -9,12 +9,14 @@ router.get('/', (req, res) => {
     res.render('home', { title: 'SoftUni Wiki' });
 });
 
-router.get('/all-articles', (req, res) => {
-    res.render('all-articles');
+router.get('/all-articles', async(req, res) => {
+    const articles = await articleService.getAllArticles();
+    res.render('all-articles', { title: 'List of articles', articles });
 });
 
-router.get('/details/:articleId', (req, res) => {
-    res.render('article');
+router.get('/details/:articleId', async(req, res) => {
+    const article = await articleService.getById(req.params.articleId);
+    res.render('article', { article });
 });
 
 router.route('/create/article')
@@ -28,6 +30,27 @@ router.route('/create/article')
             .catch((err) => {
                 res.render('create', { err });
             });
+    });
+
+router.route('/:productId/edit')
+    .get((req, res) => {
+        console.log(req.params);
+        articleService.getById(req.params.productId)
+            .then(article => {
+                res.render('edit', { article });
+            })
+            .catch(err => {
+                console.log(err);
+            });
     })
+    .post((req, res) => {
+        articleService.updateOne(req.params.productId, req.body)
+            .then(response => {
+                res.redirect(`/details/${req.params.productId}`);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
 
 module.exports = router;
--- JSBackEnd/Exams/SoftUniWiki/services/articleService.js
+++ JSBackEnd/Exams/SoftUniWiki/services/articleService.js
@@ -5,5 +5,15 @@ const Article = require('../models/article');
 exports.createArticle = async(req) => {
     const { title, description } = req.body;
     return Article.create({ title, description, articleAuthor: req.user.id });
+};
+exports.getAllArticles = async() => {
+    return Article.find().lean();
+};
 
+exports.getById = async id => {
+    return await Article.findById({ _id: id }).lean();
+};
+
+exports.updateOne = (id, articleData) => {
+    return Article.updateOne({ _id: id }, articleData);
 }
