a76db6fad79dee47bcc4397132f72ca491048d23
Bozhidar Nemski <bojonemski@gmail.com>
2021-05-21 11:42:27 +0300

    Added another problem from the Syntax Functions and Statements lab

---
+++ JSAdvanced/01.JS-Advanced-Syntax-Functions-and-Statements-Lab/02.stringLength.js
@@ -0,0 +1,8 @@
+function solve(first, second, third) {
+    const totalLength = first.length + second.length + third.length;
+    const averageLength = parseInt(totalLength / 3);
+    console.log(totalLength);
+    console.log(averageLength);
+}
+
+solve('chocolate', 'ice cream', 'cake');
