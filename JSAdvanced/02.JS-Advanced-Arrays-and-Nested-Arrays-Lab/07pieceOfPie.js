b0a0bf6acca97425114b246456813ff6bc645a7f
Bozhidar Nemski <bojonemski@gmail.com>
2021-05-27 09:52:54 +0300

    Added another problem from the Arrays-and-Nested-Arrays-Lab

---
+++ JSAdvanced/02.JS-Advanced-Arrays-and-Nested-Arrays-Lab/07pieceOfPie.js
@@ -0,0 +1,32 @@
+function solve(arr, start, end) {
+    return arr.slice(arr.indexOf(start), arr.lastIndexOf(end) + 1);
+}
+
+console.log(
+    solve(
+        [
+            'Pumpkin Pie',
+            'Key Lime Pie',
+            'Cherry Pie',
+            'Lemon Meringue Pie',
+            'Sugar Cream Pie',
+        ],
+        'Key Lime Pie',
+        'Lemon Meringue Pie'
+    )
+);
+
+console.log(
+    solve(
+        [
+            'Apple Crisp',
+            'Mississippi Mud Pie',
+            'Pot Pie',
+            'Steak and Cheese Pie',
+            'Butter Chicken Pie',
+            'Smoked Fish Pie',
+        ],
+        'Pot Pie',
+        'Smoked Fish Pie'
+    )
+);
