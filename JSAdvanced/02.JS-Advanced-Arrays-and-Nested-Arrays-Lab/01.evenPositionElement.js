6ded657b0662567f221c43d039b3094493f392e4
Bozhidar Nemski <bojonemski@gmail.com>
2021-05-27 09:05:32 +0300

    Added the first problem from the Arrays-and-Nested-Arrays-Lab

---
+++ JSAdvanced/02.JS-Advanced-Arrays-and-Nested-Arrays-Lab/01.evenPositionElement.js
@@ -0,0 +1,10 @@
+function solve(arr) {
+    let result = '';
+    for (let i = 0; i < arr.length; i += 2) {
+        result += arr[i] + ' ';
+    }
+
+    console.log(result);
+}
+
+solve(['20', '30', '40', '50', '60']);
