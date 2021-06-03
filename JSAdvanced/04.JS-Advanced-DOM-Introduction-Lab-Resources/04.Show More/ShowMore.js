01e381409dad07647aed4007cb57d19c15cee820
Bozhidar Nemski <bojonemski@gmail.com>
2021-06-03 07:15:53 +0300

    Added another problem from the Dom-Introduction-Lab

---
+++ JSAdvanced/04.JS-Advanced-DOM-Introduction-Lab-Resources/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const textEl = document.querySelector('#text');
+    const moreEl = document.querySelector('#more');
+    textEl.style.display = 'inline';
+    moreEl.style.display = 'none';
+}
