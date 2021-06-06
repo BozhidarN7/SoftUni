43b11eeae0dfc0ecbb05af33988232700915c299
Bozhidar Nemski <bojonemski@gmail.com>
2021-06-06 12:39:45 +0300

    Added another problem from DOM-Events-Lab

---
+++ JSAdvanced/05.JS-Advanced-DOM-Events-Lab-Resources/06. Dynamic-Validation/app.js
@@ -0,0 +1,12 @@
+function validate() {
+    const emailField = document.querySelector('#email');
+
+    const pattern = /[a-z]+@[a-z]+\.[a-z]+/g;
+    emailField.addEventListener('change', () => {
+        if (!emailField.value.match(pattern)) {
+            emailField.classList.add('error');
+        } else {
+            emailField.classList.remove('error');
+        }
+    });
+}
