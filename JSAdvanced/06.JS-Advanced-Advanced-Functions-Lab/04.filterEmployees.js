3e51c3c1fb16dbb837509b133195c934f3ff00e9
Bozhidar Nemski <bojonemski@gmail.com>
2021-06-10 11:47:29 +0300

    Added another problem from Advanced-Function-Lab

---
+++ JSAdvanced/06.JS-Advanced-Advanced-Functions-Lab/04.filterEmployees.js
@@ -0,0 +1,65 @@
+function solve(data, criteria) {
+    const employees = JSON.parse(data);
+
+    const [key, value] = criteria.split('-');
+    employees
+        .filter((employee) => (employee[key] ? employee[key] === value : true))
+        .forEach((employee, i) =>
+            console.log(
+                `${i}. ${employee.first_name} ${employee.last_name} - ${employee.email}`
+            )
+        );
+}
+
+// solve(
+//     `[{
+//     "id": "1",
+//     "first_name": "Ardine",
+//     "last_name": "Bassam",
+//     "email": "abassam0@cnn.com",
+//     "gender": "Female"
+//   }, {
+//     "id": "2",
+//     "first_name": "Kizzee",
+//     "last_name": "Jost",
+//     "email": "kjost1@forbes.com",
+//     "gender": "Female"
+//   },
+// {
+//     "id": "3",
+//     "first_name": "Evanne",
+//     "last_name": "Maldin",
+//     "email": "emaldin2@hostgator.com",
+//     "gender": "Male"
+//   }]`,
+//     'id-2'
+// );
+
+solve(
+    `[{
+    "id": "1",
+    "first_name": "Kaylee",
+    "last_name": "Johnson",
+    "email": "k0@cnn.com",
+    "gender": "Female"
+  }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Johnson",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+  }, {
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+  }, {
+    "id": "4",
+    "first_name": "Evanne",
+    "last_name": "Johnson",
+    "email": "ev2@hostgator.com",
+    "gender": "Male"
+  }]`,
+    'all'
+);
