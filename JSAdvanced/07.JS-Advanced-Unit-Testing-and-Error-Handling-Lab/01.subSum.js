a5a8f44511177b46f6e3d720394352e5ea72ab34
Bozhidar Nemski <bojonemski@gmail.com>
2021-06-13 12:15:48 +0300

    Added the first problem from Unit-Testing-and-Error-Handling-Lab

---
+++ JSAdvanced/07.JS-Advanced-Unit-Testing-and-Error-Handling-Lab/01.subSum.js
@@ -0,0 +1,11 @@
+function sum(arr, startIndex, endIndex) {
+    if (!Array.isArray(arr)) return NaN;
+    if (startIndex < 0) startIndex = 0;
+    if (endIndex > arr.length) endIndex = arr.length - 1;
+
+    return arr
+        .slice(startIndex, endIndex + 1)
+        .reduce((acc, el) => (acc += Number(el)), 0);
+}
+
+console.log(sum('text', 0, 2));
