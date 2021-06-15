17caba7cf137fa7e3d6840d94d741652b3e21112
Bozhidar Nemski <bojonemski@gmail.com>
2021-06-15 16:35:17 +0300

    Added the last problem from the Unit-Testing-and-Error-Handling-Exercise

---
+++ JSAdvanced/07.JS-Advanced-Unit-Testing-and-Error-Handling-Exercise/01.requestValidator.js
@@ -0,0 +1,36 @@
+function solve(req) {
+    function generateErrorMessage(wrongPart) {
+        return `Invalid request header: Invalid ${wrongPart}`;
+    }
+
+    const methods = ['GET', 'POST', 'DELETE', 'CONNECT'];
+    const versions = ['HTTP/0.9', 'HTTP/1.0', 'HTTP/1.1', 'HTTP/2.0'];
+
+    if (!req.hasOwnProperty('method') || !methods.includes(req.method)) {
+        throw new Error(generateErrorMessage('Method'));
+    }
+    if (
+        !req.hasOwnProperty('uri') ||
+        req.uri.trim() === '' ||
+        (!req.uri.match(/^[\w.]+$/) && req.uri !== '*')
+    ) {
+        throw new Error(generateErrorMessage('URI'));
+    }
+    if (!req.hasOwnProperty('version') || !versions.includes(req.version)) {
+        throw new Error(generateErrorMessage('Version'));
+    }
+    if (!req.hasOwnProperty('message') || !req.message.match(/^[^<>/\&'"]*$/)) {
+        throw new Error(generateErrorMessage('Message'));
+    }
+
+    return req;
+}
+
+console.log(
+    solve({
+        method: 'GET',
+        uri: '*',
+        version: 'HTTP/1.1',
+        message: 'asdfadfa,',
+    })
+);
