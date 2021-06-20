efbf0e3667217c883993c44e394660eb7d0a7445
Bozhidar Nemski <bojonemski@gmail.com>
2021-06-20 13:34:13 +0300

    Added another problem from Prototypes-and-Inheritance-Lab

---
+++ JSAdvanced/09.JS-Advanced-Prototypes-and-Inheritance-Lab/03.inheritingAndReplacingToString.js
@@ -0,0 +1,48 @@
+function toStringExtension() {
+    class Person {
+        constructor(name, email) {
+            this.name = name;
+            this.email = email;
+        }
+
+        toString() {
+            return `${this.constructor.name} (name: ${this.name}, email: ${this.email})`;
+        }
+    }
+
+    class Teacher extends Person {
+        constructor(name, email, subject) {
+            super(name, email);
+            this.subject = subject;
+        }
+        toString() {
+            return (
+                super.toString().slice(0, -1) + `, subject: ${this.subject})`
+            );
+        }
+    }
+
+    class Student extends Person {
+        constructor(name, email, course) {
+            super(name, email);
+            this.course = course;
+        }
+
+        toString() {
+            return super.toString().slice(0, -1) + `, course: ${this.course})`;
+        }
+    }
+
+    return {
+        Person,
+        Teacher,
+        Student,
+    };
+}
+
+const creator = toStringExtension();
+const person = new creator.Person('Ivan', 'email');
+const student = new creator.Student('Pesho', 'emailPesho', 'Math');
+
+console.log(person.toString());
+console.log(student.toString());
