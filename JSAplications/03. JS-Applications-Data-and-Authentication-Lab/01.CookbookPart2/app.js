584df86e48be40088ce159f8b7ba23e0174fbb3c
Bozhidar Nemski <bojonemski@gmail.com>
2021-07-08 17:39:24 +0300

    Added logout functionality Data-and-Authentication-Lab

--- JSAplications/03. JS-Applications-Data-and-Authentication-Lab/01.CookbookPart2/app.js
+++ JSAplications/03. JS-Applications-Data-and-Authentication-Lab/01.CookbookPart2/app.js
@@ -1,3 +1,38 @@
 const guestDiv = document.querySelector('#guest');
+const userDiv = document.querySelector('#user');
+const logoutBtn = document.querySelector('#logoutBtn');
 
-guestDiv.style.display = 'inline-block';
+const baseUrl = 'http://localhost:3030';
+
+function getToken() {
+    return localStorage.getItem('auth_token');
+}
+
+if (getToken()) {
+    userDiv.style.display = 'inline-block';
+    guestDiv.style.display = 'none';
+} else {
+    guestDiv.style.display = 'inline-block';
+    userDiv.style.display = 'none';
+}
+
+function logout() {
+    fetch(`${baseUrl}/users/logout`, {
+        method: 'GET',
+        headers: {
+            'X-Authorization': getToken(),
+        },
+    })
+        .then((response) => {
+            if (response.status === 200) {
+                localStorage.removeItem('auth_token');
+                window.location.pathname = 'index.html';
+            }
+            return response.json();
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+logoutBtn.addEventListener('click', logout);
