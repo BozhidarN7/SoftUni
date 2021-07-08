37cee13eb15cd7f3a131ed39195c157df66c1813
Bozhidar Nemski <bojonemski@gmail.com>
2021-07-08 18:31:31 +0300

    All recipes now visisble functionality Data-and-Authentication-Lab

--- JSAplications/03. JS-Applications-Data-and-Authentication-Lab/01.CookbookPart2/app.js
+++ JSAplications/03. JS-Applications-Data-and-Authentication-Lab/01.CookbookPart2/app.js
@@ -1,6 +1,7 @@
 const guestDiv = document.querySelector('#guest');
 const userDiv = document.querySelector('#user');
 const logoutBtn = document.querySelector('#logoutBtn');
+const mainEl = document.querySelector('main');
 
 const baseUrl = 'http://localhost:3030';
 
@@ -36,3 +37,123 @@ function logout() {
 }
 
 logoutBtn.addEventListener('click', logout);
+
+async function showRecipes() {
+    const recipes = await getRecipes();
+    Object.keys(recipes).forEach((id) => {
+        const recipe = recipes[id];
+        const articleEl = document.createElement('article');
+        articleEl.classList.add('preview');
+        articleEl.addEventListener('click', showInfo);
+
+        const titleDiv = document.createElement('div');
+        titleDiv.classList.add('title');
+
+        const headingEl = document.createElement('h2');
+        headingEl.textContent = `${recipe.name}`;
+        titleDiv.appendChild(headingEl);
+
+        const imgDiv = document.createElement('div');
+        imgDiv.classList.add('small');
+
+        const imgEl = document.createElement('img');
+        imgEl.src = recipe.img;
+
+        imgDiv.appendChild(imgEl);
+
+        articleEl.appendChild(titleDiv);
+        articleEl.appendChild(imgDiv);
+
+        mainEl.appendChild(articleEl);
+    });
+    mainEl.children[0].remove();
+}
+
+function getRecipes() {
+    return fetch('http://localhost:3030/data/recipes').then((response) =>
+        response.json()
+    );
+}
+async function showInfo(e) {
+    const recipeArticle = e.currentTarget;
+    const recipeName = recipeArticle.children[0].children[0].textContent;
+    const recipe = Object.values(await getRecipes()).find(
+        (curr) => Object.values(curr)[1] === recipeName
+    );
+    const id = recipe['_id'];
+    const recipeDetailsUrl = `http://localhost:3030/data/recipes/${id}`;
+    const recipeDetails = await (await fetch(recipeDetailsUrl)).json();
+
+    const articleEl = document.createElement('article');
+
+    const titleHeading = document.createElement('h2');
+    titleHeading.textContent = `${recipe.name}`;
+    articleEl.appendChild(titleHeading);
+
+    const bandDiv = document.createElement('div');
+    bandDiv.classList.add('band');
+
+    const thumbDiv = document.createElement('div');
+    thumbDiv.classList.add('thumb');
+
+    const imgEl = document.createElement('img');
+    imgEl.src = recipe.img;
+
+    thumbDiv.appendChild(imgEl);
+
+    const ingredientsDiv = document.createElement('div');
+    ingredientsDiv.classList.add('ingredients');
+
+    const ingredientsHeading = document.createElement('h3');
+    ingredientsHeading.textContent = 'Ingredients:';
+
+    const ulEl = document.createElement('ul');
+
+    if (typeof recipeDetails.ingredients == 'string') {
+        recipeDetails.ingredients.split('\n').forEach((ingr) => {
+            const liEl = document.createElement('li');
+            liEl.textContent = ingr;
+            ulEl.appendChild(liEl);
+        });
+    } else {
+        recipeDetails.ingredients.forEach((ingr) => {
+            const liEl = document.createElement('li');
+            liEl.textContent = ingr;
+            ulEl.appendChild(liEl);
+        });
+    }
+    ingredientsDiv.appendChild(ulEl);
+
+    bandDiv.appendChild(thumbDiv);
+    bandDiv.appendChild(ingredientsDiv);
+
+    const descriptionDiv = document.createElement('div');
+    descriptionDiv.classList.add('description');
+
+    const preperationHeading = document.createElement('h3');
+    preperationHeading.textContent = 'Preperation:';
+
+    descriptionDiv.appendChild(preperationHeading);
+
+    if (typeof recipeDetails.ingredients == 'string') {
+        recipeDetails.steps.split('\n').forEach((step) => {
+            const pEl = document.createElement('p');
+            pEl.textContent = step;
+            descriptionDiv.appendChild(pEl);
+        });
+    } else {
+        recipeDetails.steps.forEach((step) => {
+            const pEl = document.createElement('p');
+            pEl.textContent = step;
+            descriptionDiv.appendChild(pEl);
+        });
+    }
+
+    articleEl.appendChild(bandDiv);
+    articleEl.appendChild(descriptionDiv);
+
+    recipeArticle.insertAdjacentElement('afterend', articleEl);
+    recipeArticle.remove();
+}
+
+showRecipes();
