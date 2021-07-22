6e2126b9067b3c0a47ee1465d4bfaacf0d5858c3
Bozhidar Nemski <bojonemski@gmail.com>
2021-07-22 13:44:56 +0300

    Aded login functionality Routing-Exercise

--- JSAplications/07.JS-Applications-Routing-Execise/01.Furniture/src/pages/login.js
+++ JSAplications/07.JS-Applications-Routing-Execise/01.Furniture/src/pages/login.js
@@ -1,4 +1,5 @@
 import { render } from '../../node_modules/lit-html/lit-html.js';
+import auth from '../services/authService.js';
 
 import { loginTemplate } from '../templates/templates.js';
 
@@ -6,6 +7,11 @@ const containerDiv = document.querySelector('#root');
 
 export function initializeLogin(context) {
     renderLoginPage();
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        auth.login.call(e, context);
+    });
 }
 
 function renderLoginPage() {
--- JSAplications/07.JS-Applications-Routing-Execise/01.Furniture/src/services/authService.js
+++ JSAplications/07.JS-Applications-Routing-Execise/01.Furniture/src/services/authService.js
@@ -1,10 +1,10 @@
+import { setNavigation } from '../nav.js';
 import { jsonRequest } from './httpService.js';
 
 const baseUrl = 'http://localhost:3030/users';
 
 async function register(context) {
     this.preventDefault();
-    console.log(context);
     const data = new FormData(this.currentTarget);
     const email = data.get('email');
     const password = data.get('password');
@@ -19,6 +19,7 @@ async function register(context) {
         setAuthToken(newUser.accessToken);
         setUserId(newUser._id);
 
+        setNavigation();
         context.page.redirect('/home');
     } catch (err) {
         this.target.reset();
@@ -27,12 +28,36 @@ async function register(context) {
     }
 }
 
+async function login(context) {
+    this.preventDefault();
+    const data = new FormData(this.currentTarget);
+    const email = data.get('email');
+    const password = data.get('password');
+
+    try {
+        const newUser = await jsonRequest(`${baseUrl}/login`, 'POST', {
+            email,
+            password,
+        });
+
+        setAuthToken(newUser.accessToken);
+        setUserId(newUser._id);
+
+        setNavigation();
+        context.page.redirect('/home');
+    } catch (err) {
+        this.target.reset();
+        console.log(err);
+        context.page.redirect('/login');
+    }
+}
+
 function getUserId() {
     return localStorage.getItem('useId');
 }
 
 function getAuthToken() {
-    return localStorage.getItem('accessToken');
+    return localStorage.getItem('authToken');
 }
 
 function setAuthToken(token) {
@@ -47,6 +72,7 @@ const auth = {
     getUserId,
     getAuthToken,
     register,
+    login,
 };
 
 export default auth;
