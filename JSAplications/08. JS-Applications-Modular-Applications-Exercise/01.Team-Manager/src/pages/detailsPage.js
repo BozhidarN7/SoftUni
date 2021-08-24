8712b248b5e21eecff0b01e5df2e6499652f639c
Bozhidar Nemski <bojonemski@gmail.com>
2021-08-24 21:04:17 +0300

    Added simple logic for accepting and declineing requests Modullar-Applications-Exercise

--- JSAplications/08. JS-Applications-Modular-Applications-Exercise/01.Team-Manager/src/pages/detailsPage.js
+++ JSAplications/08. JS-Applications-Modular-Applications-Exercise/01.Team-Manager/src/pages/detailsPage.js
@@ -29,13 +29,18 @@ const detailsTemplate = (info) => html`
                                           >`
                                         : ''}`}
                               ${info.canLeave
-                                  ? html`<a href="#" class="action invert"
+                                  ? html`<a
+                                        @click=${info.cancelHandler}
+                                        href="#"
+                                        class="action invert"
                                         >Leave team</a
                                     >`
                                   : ''}
                               ${info.canCancel
                                   ? html` Membership pending.
-                                        <a href="#">Cancel request</a>`
+                                        <a @click=${info.cancelHandler} href="#"
+                                            >Cancel request</a
+                                        >`
                                   : ''}
                           </div>
                       `
@@ -55,7 +60,7 @@ const detailsTemplate = (info) => html`
                             <h3>Membership Requests</h3>
                             <ul class="tm-members">
                                 ${info.pendingMembers.map((m) =>
-                                    pendingMembersTemplate(m)
+                                    pendingMembersTemplate(m, info)
                                 )}
                             </ul>
                         </div>`
@@ -65,21 +70,56 @@ const detailsTemplate = (info) => html`
     </section>
 `;
 
-const pendingMembersTemplate = (member) => html`
+const pendingMembersTemplate = (member, info) => html`
     <li>
-        ${member.user.username}<a href="#" class="tm-control action">Approve</a
-        ><a href="#" class="tm-control action">Decline</a>
+        ${member.user.username}
+        ${info.team._ownerId === info.ownerId
+            ? html`<a
+                      @click=${info.approveHandler}
+                      href="#"
+                      class="tm-control action"
+                      >Approve</a
+                  >
+                  <a
+                      @click=${info.cancelHandler}
+                      href="#"
+                      class="tm-control action"
+                      >Decline</a
+                  >`
+            : ''}
     </li>
 `;
 
 async function joinHandler(context, teamId, e) {
     e.preventDefault();
 
-    const res = await memberService.join(teamId);
-    console.log(res);
+    const membershipResponse = await memberService.join(teamId);
+    memberService.setMembershipRequestId(membershipResponse._id);
+    getData(context);
 }
 
-export async function getDetailsPage(context) {
+async function approveHandler(context, e) {
+    e.preventDefault();
+
+    const approveResponse = await memberService.approve(
+        memberService.getMembershipRequestId()
+    );
+    console.log(approveResponse);
+    //memberService.deleteMembershipRequestId();
+    getData(context);
+}
+
+async function cancelHandler(context, e) {
+    e.preventDefault();
+    console.log(e.target.parentElement.textContent);
+    const cancelResponse = await memberService.decline(
+        memberService.getMembershipRequestId()
+    );
+    memberService.deleteMembershipRequestId();
+    getData(context);
+}
+
+async function getData(context) {
     const team = await teamService.getById(context.params.id);
     const members = await memberService.getAllMemberships(team._id);
     const joindedMembers = members.filter((m) => m.status === 'member');
@@ -112,7 +152,13 @@ export async function getDetailsPage(context) {
         canLeave,
         canCancel,
         joinHandler: joinHandler.bind(null, context, team._id),
+        cancelHandler: cancelHandler.bind(null, context),
+        approveHandler: approveHandler.bind(null, context),
     };
     const templateResult = detailsTemplate(info);
     context.renderView(templateResult);
+}
+
+export async function getDetailsPage(context) {
+    getData(context);
 }
--- JSAplications/08. JS-Applications-Modular-Applications-Exercise/01.Team-Manager/src/services/memberService.js
+++ JSAplications/08. JS-Applications-Modular-Applications-Exercise/01.Team-Manager/src/services/memberService.js
@@ -16,8 +16,39 @@ async function join(teamId) {
     return await jsonRequest(`${baseUrl}`, 'POST', { teamId }, true, false);
 }
 
+async function decline(requestId) {
+    return await jsonRequest(`${baseUrl}/${requestId}`, 'DELETE', true, true);
+}
+
+async function approve(requestId) {
+    return await jsonRequest(
+        `${baseUrl}/${requestId}`,
+        'PUT',
+        { status: 'member' },
+        true,
+        true
+    );
+}
+
+function setMembershipRequestId(id) {
+    localStorage.setItem('membershipRequestId', id);
+}
+
+function getMembershipRequestId() {
+    return localStorage.getItem('membershipRequestId');
+}
+
+function deleteMembershipRequestId() {
+    localStorage.removeItem('membershipRequestId');
+}
+
 export default {
     getAll,
     getAllMemberships,
     join,
+    decline,
+    approve,
+    setMembershipRequestId,
+    getMembershipRequestId,
+    deleteMembershipRequestId,
 };
