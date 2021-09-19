2e7986bcdc5d544aee49b9306d0f83f3bf76d57d
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-09-19 10:13:49 +0300

    Added Another solution to problem 10 CRUD Exercises

--- MSSQL/02.CRUD-Exercises/AllProblems.sql
+++ MSSQL/02.CRUD-Exercises/AllProblems.sql
@@ -38,6 +38,9 @@ SELECT [FirstName], [LastName], [JobTitle] FROM [Employees] WHERE [Salary] BETWE
 SELECT [FirstName] + ' ' + [MiddleName] + ' ' + [LastName] AS [Full Name] FROM [Employees]
 WHERE [Salary] = 25000 OR [Salary] = 14000 OR [Salary] = 12500 OR [Salary] = 23600
 
+SELECT [FirstName] + ' ' + [MiddleName] + ' ' + [LastName] AS [Full Name] FROM [Employees]
+WHERE [Salary] IN (25000, 14000, 12500, 23600)
+
 -- Problem 11
 
 SELECT [FirstName], [LastName] FROM [Employees] WHERE [ManagerId] IS NULL
