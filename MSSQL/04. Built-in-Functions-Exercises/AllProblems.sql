2db0d817dea62b4c3c76be0d5cd3369b2423176f
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-09-26 23:42:08 +0300

    Added problems till problem 17 Built-in-Functions-Exercise

--- MSSQL/04. Built-in-Functions-Exercises/AllProblems.sql
+++ MSSQL/04. Built-in-Functions-Exercises/AllProblems.sql
@@ -51,6 +51,57 @@ WHERE DATEPART(YEAR, [HireDate]) > 2000
 SELECT[FirstName], [LastName] FROM [Employees]
 WHERE LEN([LastName]) = 5
 
+-- Problem 10
+SELECT 
+	  [EmployeeID]
+	, [FirstName]
+	, [LastName]
+	, [Salary] 
+	, DENSE_RANK() OVER (PARTITION BY [Salary] ORDER BY [EmployeeID]) AS [Rank]
+	FROM [Employees] 
+	WHERE [Salary] BETWEEN 10000 AND 50000
+	ORDER BY [Salary] DESC
+
+-- Problem 11
+SELECT * FROM 
+(SELECT 
+	  [EmployeeID]
+	, [FirstName]
+	, [LastName]
+	, [Salary] 
+	, DENSE_RANK() OVER (PARTITION BY [Salary] ORDER BY [EmployeeID]) AS [Rank]
+	FROM [Employees] 
+	WHERE [Salary] BETWEEN 10000 AND 50000)
+AS [RankingTable]
+WHERE Rank = 2
+ORDER BY [Salary] DESC	
+
 
 -- Problem 12
-SELECT * FROM [Countries]
+
+SELECT [CountryName] AS 'Country Name', [IsoCode] AS 'ISO Code' FROM [Countries]
+WHERE [CountryName] LIKE '%a%a%a%'
+ORDER BY [IsoCode]
+
+-- Problem 13
+SELECT [p].[PeakName], [r].[RiverName], LOWER(CONCAT(LEFT([p].[PeakName], LEN([p].[PeakName]) - 1),[r].[RiverName])) AS [Mix] FROM [Peaks] AS p, [Rivers] as r
+WHERE RIGHT([p].[PeakName], 1) = LEFT([r].[RiverName], 1)
+ORDER BY [Mix]
+
+-- Problem 14
+
+SELECT TOP (50) [Name], FORMAT(CAST([Start] AS DATE), 'yyyy-MM-dd') AS [Start] FROM [Games]
+WHERE DATEPART(YEAR, [Start]) = 2011 OR DATEPART(YEAR, [Start]) = 2012
+ORDER BY [Start] ,[Name]
+
+-- Problem 15
+
+SELECT [Username], RIGHT([Email],LEN([Email]) - CHARINDEX('@', [Email])) AS [Email Provider] FROM [Users]
+ORDER BY [Email Provider], [Username]
+
+-- Problem 16
+SELECT [Username], [IpAddress] AS [IP Address] FROM [Users]
+WHERE [IpAddress] LIKE '___.1%%.___'
+ORDER BY [Username]
+
+-- Problem 17
