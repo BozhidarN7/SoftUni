32b0d0b74714bab5df4c7fda371d7df6510a333f
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-09-29 23:12:32 +0300

    Added till problem 15 Subqueries And Joins

--- MSSQL/05.Subqueries-and-JOINs-Exercises/AllProblems.sql
+++ MSSQL/05.Subqueries-and-JOINs-Exercises/AllProblems.sql
@@ -28,7 +28,101 @@ ON e.[DepartmentID] = d.[DepartmentID]
 WHERE e.[Salary] > 15000
 ORDER BY  d.[DepartmentID]
 
-SELECT * FROM [Employees]
-SELECT * FROM [Addresses]
-SELECT * FROM [Towns]
-SELECT * FROM [Departments]
+-- Problem 5
+
+SELECT TOP (3) e.[EmployeeID], e.[FirstName] FROM [Employees] AS e
+LEFT JOIN [EmployeesProjects] AS ep
+ON  e.[EmployeeID] = ep.[EmployeeID]
+WHERE ep.[ProjectID] IS NULL
+ORDER BY e.[EmployeeID]
+
+-- Problem 6
+SELECT e.[FirstName], e.[LastName], e.[HireDate], d.[Name] AS [DeptName] FROM [Employees] AS e
+JOIN [Departments] AS d
+ON e.[DepartmentID] = d.[DepartmentID]
+WHERE e.[HireDate] > '1.1.1999' AND d.[Name] IN ('Finance', 'Sales')
+ORDER BY e.[HireDate]
+
+-- Problem 7
+SELECT TOP (5) e.[EmployeeID], e.[FirstName], p.[Name] AS [ProjectName] FROM [Employees] AS e
+JOIN [EmployeesProjects] AS ep
+ON e.[EmployeeID] = ep.[EmployeeID]
+JOIN [Projects] AS p
+ON ep.[ProjectID] = p.[ProjectID]
+WHERE p.[EndDate] IS NULL AND p.[StartDate] > '08.13.2002'
+ORDER BY e.[EmployeeID]
+
+--Problem 8
+SELECT e.[EmployeeID], e.[FirstName], 
+CASE
+	WHEN YEAR(p.[StartDate]) >= 2005 THEN NULL
+	ELSE p.[Name]
+END AS [ProjectName]
+FROM [Employees] AS e
+LEFT JOIN [EmployeesProjects] AS ep
+ON e.[EmployeeID] = ep.[EmployeeID]
+LEFT JOIN [Projects] AS p
+ON ep.[ProjectID] = p.[ProjectID]
+WHERE e.[EmployeeID] = 24
+
+-- Problem 9
+SELECT a.[EmployeeID], a.[FirstName], a.[ManagerID], b.[FirstName] AS [ManagerName]
+FROM [Employees] AS a
+JOIN [Employees] as b
+ON a.[ManagerID] = b.[EmployeeID]
+WHERE a.[ManagerID] IN (3, 7)
+ORDER BY a.[EmployeeID]
+
+-- Problem 10
+SELECT TOP (50) 
+e.[EmployeeID]
+, CONCAT(e.[FirstName], ' ',e.[LastName]) AS [EmployeeName]
+, CONCAT(m.[FirstName], ' ', m.[LastName]) AS [ManagerName]
+, d.[Name] AS [DepartmentName]
+FROM [Employees] AS e
+JOIN [Employees] AS m
+ON e.[ManagerID] = m.[EmployeeID]
+JOIN [Departments] AS d
+ON e.[DepartmentID] = d.[DepartmentID]
+ORDER BY e.[EmployeeID]
+
+-- Problem 11
+SELECT TOP (1) AVG(e.[Salary]) AS [MinAverageSalary] FROM [Employees] AS e
+JOIN [Departments] AS d
+ON e.[DepartmentID] = d.[DepartmentID]
+GROUP BY d.[Name]
+ORDER BY [MinAverageSalary]
+
+-- Problem 12
+SELECT mc.[CountryCode], m.[MountainRange], p.[PeakName], p.[Elevation] FROM [Peaks] AS p
+LEFT JOIN [Mountains] AS m
+ON p.[MountainId] = m.[Id]
+LEFT JOIN [MountainsCountries] AS mc
+ON m.[Id] = mc.[MountainId]
+WHERE p.Elevation > 2835 AND mc.[CountryCode] = 'BG'
+ORDER BY p.[Elevation] DESC
+
+-- Problem 13
+
+SELECT c.[CountryCode], COUNT(mc.[MountainId]) AS [MountainRanges] FROM [Countries] AS c
+LEFT JOIN [MountainsCountries] AS mc
+ON c.[CountryCode] = mc.[CountryCode]
+WHERE c.[CountryCode] IN ('BG', 'US', 'RU')
+GROUP BY c.[CountryCode]
+
+-- Problem 14
+SELECT TOP (5) c.[CountryName], r.[RiverName] FROM [Countries] AS c
+LEFT JOIN [CountriesRivers] AS cr
+ON c.[CountryCode] = cr.[CountryCode]
+LEFT JOIN [Rivers] AS r
+ON cr.[RiverId] = r.[Id]
+WHERE c.[ContinentCode] = 'AF'
+ORDER BY c.[CountryName]
+
+SELECT * FROM [Peaks]
+SELECT * FROM [Mountains]
+SELECT * FROM [Countries]
+SELECT * FROM [MountainsCountries]
+SELECT * FROM [Rivers]
+SELECT * FROM [Continents]
+
