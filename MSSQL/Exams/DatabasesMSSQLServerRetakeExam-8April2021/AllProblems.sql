214f68d17c1fa9379da23e37366662aa962fe9f6
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-10-10 23:48:21 +0300

    Added first problem from the exam

---
+++ MSSQL/Exams/DatabasesMSSQLServerRetakeExam-8April2021/AllProblems.sql
@@ -0,0 +1,58 @@
+-- Problem 1
+
+CREATE DATABASE [Service]
+USE [Service]
+
+CREATE TABLE [Users]
+(
+	[Id] INT PRIMARY KEY IDENTITY NOT NULL,
+	[Username] VARCHAR(30) NOT NULL UNIQUE,
+	[Password] VARCHAR(50) NOT NULL,
+	[Name] VARCHAR(50),	
+	[Birthdate] DATETIME2,
+	[Age] INT,
+	CHECK ([Age] BETWEEN 14 AND 110),
+	[Email] VARCHAR(50) NOT NULL 
+)
+
+CREATE TABLE [Departments]
+(
+	[Id] INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE [Employees]
+(
+	[Id] INT PRIMARY KEY IDENTITY NOT NULL,
+	[FirstName] VARCHAR(25),
+	[LastName] VARCHAR(25),
+	[Birthdate] DATETIME2,
+	[Age] INT,
+	CHECK ([Age] BETWEEN 18 AND 110),
+	[DepartmentId] INT FOREIGN KEY REFERENCES [Departments]([Id])
+)
+
+CREATE TABLE [Categories]
+(
+	[Id] INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] VARCHAR(50) NOT NULL,
+	[DepartmentId] INT FOREIGN KEY REFERENCES [Departments]([Id]) NOT NULL
+)
+
+CREATE TABLE [Status]
+(
+	[Id] INT PRIMARY KEY IDENTITY NOT NULL,
+	[Label] VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE [Reports]
+(
+	[Id] INT PRIMARY KEY IDENTITY NOT NULL,
+	[CategoryId] INT FOREIGN KEY REFERENCES [Categories]([Id]) NOT NULL,
+	[StatusId] INT FOREIGN KEY REFERENCES [Status]([Id]) NOT NULL,
+	[OpenDate] DATETIME2 NOT NULL,
+	[CloseDate] DATETIME2,
+	[Description] VARCHAR(200) NOT NULL,
+	[UserId] INT FOREIGN KEY REFERENCES [Users]([Id]) NOT NULL,
+	[EmployeeId] INT FOREIGN KEY REFERENCES [Employees]([Id])
+)
