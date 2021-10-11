742d8319998523758dbff2e77f2d10fdfb3a391a
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-10-11 23:52:34 +0300

    Added few more problems

--- MSSQL/Exams/DatabasesMSSQLServerRetakeExam-8April2021/AllProblems.sql
+++ MSSQL/Exams/DatabasesMSSQLServerRetakeExam-8April2021/AllProblems.sql
@@ -56,3 +56,56 @@ CREATE TABLE [Reports]
 	[UserId] INT FOREIGN KEY REFERENCES [Users]([Id]) NOT NULL,
 	[EmployeeId] INT FOREIGN KEY REFERENCES [Employees]([Id])
 )
+
+-- Problem 2
+INSERT INTO [Employees] ([FirstName], [LastName], [Birthdate], [DepartmentId]) VALUES
+('Marlo', 'O''Malley', '1958-9-21', 1),
+('Niki', 'Stanaghan', '1969-11-26', 4),
+('Ayrton', 'Senna', '1960-03-21', 9),
+('Ronnie', 'Peterson', '1944-02-14', 9),
+('Giovanna', 'Amati', '1959-07-20', 5)
+
+INSERT INTO [Reports] ([CategoryId], [StatusId], [OpenDate], [CloseDate], [Description], [UserId], [EmployeeId]) VALUES 
+(1, 1, '2017-04-13', NULL, 'Stuck Road on Str.133', 6, 2),
+(6, 3, '2015-09-05', '2015-12-06', 'Charity trail running', 3, 5),
+(14, 2, '2015-09-07', NULL, 'Falling bricks on Str.58', 5, 2),
+(4, 3, '2017-07-03', '2017-07-06', 'Cut off streetlight on Str.11', 1, 1)
+
+-- Problem 3
+UPDATE [Reports]
+SET [CloseDate] = GETDATE()
+WHERE [CloseDate] IS NULL
+
+-- Problem 4
+DELETE FROM [Reports]
+WHERE [StatusId] = 4
+
+-- Problem 5
+SELECT [Description], FORMAT([OpenDate], 'dd-MM-yyyy') AS [OpenDate] FROM [Reports] AS r
+WHERE [EmployeeId] IS NULL
+ORDER BY r.[OpenDate], [Description]
+
+-- Problem 6
+SELECT r.[Description], c.[Name] AS [CategoryName] FROM [Reports] AS r
+LEFT JOIN [Categories] as c
+ON r.[CategoryId] = c.[Id]
+WHERE r.[CategoryId] IS NOT NULL
+ORDER BY r.[Description], c.[Name] 
+
+-- Problem 7
+SELECT TOP(5) c.[Name] AS [CategoryName], COUNT(r.[CategoryId]) AS [ReportsNumber] FROM [Reports] as r
+LEFT JOIN [Categories] as c
+ON r.[CategoryId] = c.[Id]
+GROUP BY c.[Name]
+ORDER BY [ReportsNumber] DESC, c.[Name]
+
+-- Problem 8
+SELECT u.[Username], c.[Name] AS [CategoryName] FROM [Reports] as r
+JOIN [Users] AS u
+ON r.[UserId] = u.[Id]
+JOIN [Categories] AS c
+ON r.[CategoryId] = c.[Id]
+WHERE  r.[OpenDate] = u.[Birthdate]
+ORDER BY u.[Username], c.[Name]
+
+SELECT FORMAT([OpenDate], 'dd-MM-yyyy') FROM [Reports]
