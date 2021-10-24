24a5a77410e73a397aa203a983b0afbb1a993c9f
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-10-24 17:54:32 +0300

    Started implementing DbContext class ORM-Fundamental-Exercise

--- EntityFrameworkCore/02.ORM-Fundamentals-MiniORM-Exercise/MiniORM/DbContext.cs
+++ EntityFrameworkCore/02.ORM-Fundamentals-MiniORM-Exercise/MiniORM/DbContext.cs
@@ -1,9 +1,148 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
 
 namespace MiniORM
 {
-    internal class DbContext
+    public abstract class DbContext
     {
-        public static Type [] AllowedSqlTypes { get; set; }
+        private readonly DatabaseConnection connection;
+        private readonly Dictionary<Type, PropertyInfo> dbSetProperties;
+
+        internal static readonly Type[] AllowedSqlTypes = {
+            typeof(string),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(decimal),
+            typeof(bool),
+            typeof(DateTime)
+        };
+
+        protected DbContext(string connectionString)
+        {
+            connection = new DatabaseConnection(connectionString);
+
+            dbSetProperties = DiscoverDbSets();
+
+            using (new ConnectionManager(connection))
+            {
+                InitializeDbSets();
+            }
+
+            this.MapAllRelations();
+        }
+
+        private void MapAllRelations()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void InitializeDbSets()
+        {
+            foreach (KeyValuePair<Type, PropertyInfo> dbSet in dbSetProperties)
+            {
+                Type dbSetType = dbSet.Key;
+                PropertyInfo dbSetProperty = dbSet.Value;
+
+                MethodInfo populateDbSetGeneric = typeof(DbContext)
+                    .GetMethod("PopulateDbSet", BindingFlags.Instance | BindingFlags.NonPublic)
+                    .MakeGenericMethod(dbSetType);
+
+                populateDbSetGeneric.Invoke(this, new object[] { dbSetProperty });
+            }
+        }
+
+        private Dictionary<Type, PropertyInfo> DiscoverDbSets()
+        {
+            throw new NotImplementedException();
+        }
+        private bool IsObjectValid(object entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Persist<TEntity>(DbSet<TEntity> dbSet) where TEntity : class, new()
+        {
+            string tableName = GetTableName(typeof(TEntity));
+            string[] columns = connection.FetchColumnNames(tableName).ToArray();
+
+            if (dbSet.ChangeTracker.Added.Any())
+            {
+                connection.InsertEntities(dbSet.ChangeTracker.Added, tableName, columns);
+            }
+
+            TEntity[] modifiedEntities = dbSet.ChangeTracker.GetModifiedEntities(dbSet).ToArray();
+            if (modifiedEntities.Any())
+            {
+                connection.UpdateEntities(modifiedEntities, tableName, columns);
+            }
+
+            if (dbSet.ChangeTracker.Removed.Any())
+            {
+                connection.DeleteEntities(dbSet.ChangeTracker.Removed, tableName, columns); 
+            }
+        }
+
+        private string GetTableName(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChagnes()
+        {
+            object[] dbSets = dbSetProperties.Select(pi => pi.Value.GetValue(this)).ToArray();
+
+            foreach (IEnumerable<object> dbSet in dbSets)
+            {
+                object[] invalidEntities = dbSet.Where(entity => !IsObjectValid(entity)).ToArray();
+
+                if (invalidEntities.Any())
+                {
+                    throw new InvalidOperationException($"{invalidEntities.Length} Invalid Entities found in {dbSet.GetType().Name}!");
+                }
+            }
+
+            using (new ConnectionManager(connection))
+            {
+                using (SqlTransaction transaction = connection.StartTransaction())
+                {
+                    foreach (IEnumerable dbSet in dbSets)
+                    {
+                        Type dbSetType = dbSet.GetType().GetGenericArguments().First();
+
+                        MethodInfo persistMethod = typeof(DbContext)
+                            .GetMethod("Persist", BindingFlags.Instance | BindingFlags.NonPublic)
+                            .MakeGenericMethod(dbSetType);
+
+                        try
+                        {
+                            persistMethod.Invoke(this, new object[] { dbSet });
+                        }
+                        catch (TargetInvocationException tie)
+                        {
+                            throw tie.InnerException;
+                        }
+                        catch (InvalidOperationException)
+                        {
+                            transaction.Rollback();
+                            throw;
+                        }
+                        catch (SqlException)
+                        {
+                            transaction.Rollback();
+                            throw;
+                        }
+                    }
+                    transaction.Commit();
+                }
+            }
+
+        }
+
     }
 }
