b90e89b3bceb36b5a90bb6881b69d44f90c7958f
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-10-24 16:32:10 +0300

    Implemented DbSet class ORM-Fundamental-Exercise

--- EntityFrameworkCore/02.ORM-Fundamentals-MiniORM-Exercise/MiniORM/DbSet.cs
+++ EntityFrameworkCore/02.ORM-Fundamentals-MiniORM-Exercise/MiniORM/DbSet.cs
@@ -1,10 +1,81 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MiniORM
 {
-	internal class DbSet<TEntity>
+    public class DbSet<TEntity> : ICollection<TEntity> where TEntity : class, new()
     {
-		internal IList<TEntity> Entities { get; set; }
+        internal ChangeTracker<TEntity> ChangeTracker { get; set; }
+        internal IList<TEntity> Entities { get; set; }
 
+        internal DbSet(IEnumerable<TEntity> entities)
+        {
+            Entities = entities.ToList();
+            ChangeTracker = new ChangeTracker<TEntity>(entities);
+        }
+        public int Count => Entities.Count;
+
+        public bool IsReadOnly => Entities.IsReadOnly;
+
+        public void Add(TEntity item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item), "Item cannot be null!");
+            }
+
+            Entities.Add(item);
+            ChangeTracker.Add(item);
+        }
+
+        public void Clear()
+        {
+            while (Entities.Any())
+            {
+                TEntity entity = Entities.First();
+                Remove(entity);
+            }
+        }
+
+        public bool Contains(TEntity item) => Entities.Contains(item);
+
+        public void CopyTo(TEntity[] array, int arrayIndex) => Entities.CopyTo(array, arrayIndex);
+
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            return Entities.GetEnumerator();
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            foreach (TEntity entity in entities.ToArray())
+            {
+                Remove(entity);
+            }
+        }
+
+        public bool Remove(TEntity item)
+        {
+            if (item is null)
+            {
+                throw new ArgumentNullException(nameof(item), "Item cannot be null!");
+            }
+
+            bool removedSuccessfully = Entities.Remove(item);
+
+            if (removedSuccessfully)
+            {
+                ChangeTracker.Remove(item);
+            }
+
+            return removedSuccessfully;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
