b5271000817d8fe07ae9b299607285ab9fa4c580
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-10-28 16:57:12 +0300

    Added another problem from EntityFrameworkIntroduction exercise

--- EntityFrameworkCore/03. Entity-Framework-Introduction-Exercises/03.EntityFrameworkIntroductionExercises/SoftUni/StartUp.cs
+++ EntityFrameworkCore/03. Entity-Framework-Introduction-Exercises/03.EntityFrameworkIntroductionExercises/SoftUni/StartUp.cs
@@ -1,4 +1,5 @@
-﻿using SoftUni.Data;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using SoftUni.Data;
 using SoftUni.Models;
 using System;
 using System.Linq;
@@ -11,7 +12,7 @@ namespace SoftUni
         {
             using (SoftUniContext context = new SoftUniContext())
             {
-                Console.WriteLine(GetEmployeesFromResearchAndDevelopment(context));
+                Console.WriteLine(AddNewAddressToEmployee(context));
 
             }
         }
@@ -50,6 +51,21 @@ namespace SoftUni
                 .OrderBy(e => e.Salary)
                 .ThenByDescending(e => e.FirstName)
                 .Select(e => $"{e.FirstName} {e.LastName} from {e.DepartmentName} - ${e.Salary:f2}")
+                .ToList());
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            EntityEntry<Address> newAddress1 = context.Addresses.Add(new Address() { AddressText = "Vitoshka 15", TownId = 4 });
+
+            Employee employee = context.Employees.FirstOrDefault(x => x.LastName == "Nakov");
+            employee.Address = newAddress1.Entity;
+
+            context.SaveChanges();
+
+            return String.Join(Environment.NewLine, context.Employees
+                .OrderByDescending(e => e.AddressId)
+                .Take(10).Select(e => e.Address.AddressText)
                 .ToList());
         }
 
