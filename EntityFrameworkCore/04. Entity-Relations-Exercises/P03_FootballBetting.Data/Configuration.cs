c6b99bf3c41ed2670fff82bc5531a235a0d952ee
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-10-31 12:35:53 +0200

    Added second problem from Entity-Relations-Exercises

---
+++ EntityFrameworkCore/04. Entity-Relations-Exercises/P03_FootballBetting.Data/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data
+{
+    public static class Configuration
+    {
+        public const string CONNECTION_STRING = @"Server=DESKTOP-7SFRFQN\SQLEXPRESS;Database=Bet366;Integrated Security=true";
+    }
+}
---
+++ EntityFrameworkCore/04. Entity-Relations-Exercises/P03_FootballBetting/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_FootballBetting.Data;
+using System;
+
+namespace P03_FootballBetting
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            FootballBettingContext db = new FootballBettingContext();
+
+            db.Database.Migrate();
+
+            Console.WriteLine("Database created successfully!");
+            
+        }
+    }
+}
