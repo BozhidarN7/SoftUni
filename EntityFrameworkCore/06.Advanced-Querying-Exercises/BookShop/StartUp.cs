1ffc1f4d73e17d360fa9215b01610384e68c2c98
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-11-07 12:01:18 +0200

    Added first problem from Advanced-Querying-Exercises

---
+++ EntityFrameworkCore/06.Advanced-Querying-Exercises/BookShop/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            string command = Console.ReadLine();
+            Console.WriteLine(GetBooksByAgeRestriction(db, command));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            return string.Join(Environment.NewLine, context.Books
+                 .Where(b => Enum.Parse<AgeRestriction>(command, true) == b.AgeRestriction)
+                 .Select(b => b.Title)
+                 .OrderBy(t => t)
+                 .ToList());
+        }
+    }
+}
