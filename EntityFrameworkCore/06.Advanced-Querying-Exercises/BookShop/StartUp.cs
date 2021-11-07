2ce4e7505654d96d60e7d39c191612ba54631bb6
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-11-07 12:18:39 +0200

    Added another problem from Advanced-Querying-Exercises

--- EntityFrameworkCore/06.Advanced-Querying-Exercises/BookShop/StartUp.cs
+++ EntityFrameworkCore/06.Advanced-Querying-Exercises/BookShop/StartUp.cs
@@ -13,8 +13,8 @@
             using var db = new BookShopContext();
             //DbInitializer.ResetDatabase(db);
 
-            //string command = Console.ReadLine();
-            Console.WriteLine(GetBooksByPrice(db));
+            string command = Console.ReadLine();
+            Console.WriteLine(GetBooksNotReleasedIn(db,int.Parse(command)));
         }
 
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
@@ -40,6 +40,15 @@
                 Where(b => b.Price > 40)
                 .OrderByDescending(b => b.Price)
                 .Select(b => $"{b.Title} - ${b.Price:f2}")
+                .ToList());
+        }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            return string.Join(Environment.NewLine, context.Books
+                .Where(b => b.ReleaseDate.Value.Year != year)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
                 .ToList());
         }
     }
