4eb4a1d0e30b21b9574eac182f7df6240fcf6496
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-11-13 09:16:31 +0200

    Added another problem from JSON-Processing-Exercises

--- EntityFrameworkCore/08.JSON-Processing-Exercises/ProductShop/ProductShop/StartUp.cs
+++ EntityFrameworkCore/08.JSON-Processing-Exercises/ProductShop/ProductShop/StartUp.cs
@@ -14,7 +14,7 @@ namespace ProductShop
         {
             ProductShopContext context = new ProductShopContext();
             string userInput = File.ReadAllText("../../../Datasets/products.json");
-            Console.WriteLine(ImportProducts(context, userInput));
+            Console.WriteLine(ImportCategories(context, userInput));
         }
 
         public static string ImportUsers(ProductShopContext context, string inputJson)
@@ -25,7 +25,7 @@ namespace ProductShop
             return $"Successfully imported {users.Count}";
         }
 
-        public static string ImportProducts(ProductShopContext context,string inputJson)
+        public static string ImportProducts(ProductShopContext context, string inputJson)
         {
             List<Product> products = JsonConvert.DeserializeObject<List<Product>>(inputJson);
             context.Products.AddRange(products);
@@ -33,5 +33,19 @@ namespace ProductShop
 
             return $"Successfully imported {products.Count}";
         }
+
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            List<Category> categories = JsonConvert.DeserializeObject<List<Category>>(inputJson)
+                .Where(c => c.Name != null && c.Name != String.Empty)
+                .ToList();
+
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Count}";
+        }
+
+
     }
 }
