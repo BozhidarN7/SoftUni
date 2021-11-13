4327701934b4fc1aa35cbe1d1ff1ca8a7014f6ef
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-11-13 08:39:51 +0200

    Added the first problem from JSON-Processing-Exercises

---
+++ EntityFrameworkCore/08.JSON-Processing-Exercises/ProductShop/ProductShop/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            ProductShopContext context = new ProductShopContext();
+            string userInput = File.ReadAllText("../../../Datasets/users.json");
+            Console.WriteLine(ImportUsers(context, userInput));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            List<User> users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+            context.Users.AddRange(users);
+            context.SaveChanges();
+            return $"Successfully imported {users.Count}";
+        }
+    }
+}
