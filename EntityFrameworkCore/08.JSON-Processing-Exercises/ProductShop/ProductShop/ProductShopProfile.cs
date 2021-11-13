2b6cdbaed48074d4b835c145dc21d6c5e7b81ca0
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-11-13 17:53:44 +0200

    Added another problem from JSON-Processing-Exercises

--- EntityFrameworkCore/08.JSON-Processing-Exercises/ProductShop/ProductShop/ProductShopProfile.cs
+++ EntityFrameworkCore/08.JSON-Processing-Exercises/ProductShop/ProductShop/ProductShopProfile.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using ProductShop.DataTransferObjects;
+using ProductShop.DataTransferObjects.Input;
+using ProductShop.Models;
 
 namespace ProductShop
 {
@@ -6,6 +9,10 @@ namespace ProductShop
     {
         public ProductShopProfile()
         {
+            CreateMap<UserInputDto, User>();
+            CreateMap<ProductInputDto, Product>();
+            CreateMap<CategoryInputDto, Category>();
+            CreateMap<CategoryProductInputDto, CategoryProduct>();
         }
     }
 }
--- EntityFrameworkCore/08.JSON-Processing-Exercises/ProductShop/ProductShop/StartUp.cs
+++ EntityFrameworkCore/08.JSON-Processing-Exercises/ProductShop/ProductShop/StartUp.cs
@@ -2,33 +2,47 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using AutoMapper;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using ProductShop.Data;
+using ProductShop.DataTransferObjects.Input;
 using ProductShop.Models;
 
 namespace ProductShop
 {
     public class StartUp
     {
+        static IMapper mapper;
         public static void Main(string[] args)
         {
             ProductShopContext context = new ProductShopContext();
-            string userInput = File.ReadAllText("../../../Datasets/products.json");
-            Console.WriteLine(ImportCategories(context, userInput));
+
+            //string categoriesProductsInput = File.ReadAllText("../../../Datasets/categories-products.json");
+
+            Console.WriteLine(GetSoldProducts(context));
         }
 
         public static string ImportUsers(ProductShopContext context, string inputJson)
         {
-            List<User> users = JsonConvert.DeserializeObject<List<User>>(inputJson);
-            context.Users.AddRange(users);
+            InitializeMapper();
+
+            List<UserInputDto> users = JsonConvert.DeserializeObject<List<UserInputDto>>(inputJson);
+            List<User> mappedUseres = mapper.Map<List<User>>(users);
+
+            context.Users.AddRange(mappedUseres);
             context.SaveChanges();
+
             return $"Successfully imported {users.Count}";
         }
 
         public static string ImportProducts(ProductShopContext context, string inputJson)
         {
-            List<Product> products = JsonConvert.DeserializeObject<List<Product>>(inputJson);
-            context.Products.AddRange(products);
+            InitializeMapper();
+            List<ProductInputDto> products = JsonConvert.DeserializeObject<List<ProductInputDto>>(inputJson);
+            List<Product> mappedProducts = mapper.Map<List<Product>>(products);
+
+            context.Products.AddRange(mappedProducts);
             context.SaveChanges();
 
             return $"Successfully imported {products.Count}";
@@ -36,16 +50,94 @@ namespace ProductShop
 
         public static string ImportCategories(ProductShopContext context, string inputJson)
         {
-            List<Category> categories = JsonConvert.DeserializeObject<List<Category>>(inputJson)
+            InitializeMapper();
+            List<CategoryInputDto> categories = JsonConvert.DeserializeObject<List<CategoryInputDto>>(inputJson)
                 .Where(c => c.Name != null && c.Name != String.Empty)
                 .ToList();
+            List<Category> mappedCategories = mapper.Map<List<Category>>(categories);
 
-            context.Categories.AddRange(categories);
+            context.Categories.AddRange(mappedCategories);
             context.SaveChanges();
 
             return $"Successfully imported {categories.Count}";
         }
 
+        public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
+        {
+            InitializeMapper();
+            List<CategoryProductInputDto> categoriesProducts = JsonConvert
+                .DeserializeObject<List<CategoryProductInputDto>>(inputJson);
+            List<CategoryProduct> mappedCategoriesProducts = mapper.Map<List<CategoryProduct>>(categoriesProducts);
 
+            context.CategoryProducts.AddRange(mappedCategoriesProducts);
+            context.SaveChanges();
+
+            return $"Successfully imported {categoriesProducts.Count}";
+        }
+
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var products = context.Products
+                .Where(p => p.Price >= 500 && p.Price < 1000)
+                .OrderBy(p => p.Price)
+                .Select(p => new
+                {
+                    p.Name,
+                    p.Price,
+                    Seller = $"{p.Seller.FirstName} {p.Seller.LastName}",
+                })
+                .ToList();
+
+            string serialized = JsonConvert.SerializeObject(products, new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new DefaultContractResolver()
+                {
+                    NamingStrategy = new SnakeCaseNamingStrategy()
+                }
+            }); ;
+            return serialized;
+        }
+
+        public static string GetSoldProducts(ProductShopContext context)
+        {
+            var users = context.Users
+                .Where(u => u.ProductsBought.Count() > 1)
+                .OrderBy(u => u.LastName)
+                .ThenBy(u => u.FirstName)
+                .Select(u => new
+                {
+                    u.FirstName,
+                    u.LastName,
+                    soldProducts = u.ProductsBought.Select(p => new
+                    {
+                        p.Name,
+                        p.Price,
+                        BuyerFirstName = p.Buyer.FirstName,
+                        BuyerLastName = p.Buyer.LastName
+                    })
+                });
+
+            string serialized = JsonConvert.SerializeObject(users, new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new DefaultContractResolver()
+                {
+                    NamingStrategy = new CamelCaseNamingStrategy()
+                }
+            });
+
+            return serialized;
+        }
+
+        private static void InitializeMapper()
+        {
+            MapperConfiguration mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            });
+
+            mapper = new Mapper(mapperConfiguration);
+        }
     }
 }
