852f7941772b9072e177190b3f43ac8b24c59d7c
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-11-21 18:06:31 +0200

    Added another problem from XML-Processing-Exercises

---
+++ EntityFrameworkCore/09. XML-Processing-Exercises/CarDealer/DTO/ImportDto/ImportCarPartDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTO.ImportDto
+{
+    [XmlType("partId")]
+    public class ImportCarPartDto
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+    }
+}
--- EntityFrameworkCore/09. XML-Processing-Exercises/CarDealer/StartUp.cs
+++ EntityFrameworkCore/09. XML-Processing-Exercises/CarDealer/StartUp.cs
@@ -20,8 +20,11 @@ namespace CarDealer
 
             string suppliersInput = File.ReadAllText("../../../Datasets/suppliers.xml");
             string partsInput = File.ReadAllText("../../../Datasets/parts.xml");
+            string carsInput = File.ReadAllText("../../../Datasets/cars.xml");
+
             Console.WriteLine(ImportSuppliers(context, suppliersInput));
             Console.WriteLine(ImportParts(context, partsInput));
+            Console.WriteLine(ImportCars(context, carsInput));
         }
 
         public static string ImportSuppliers(CarDealerContext context, string inputXml)
@@ -82,6 +85,53 @@ namespace CarDealer
             context.SaveChanges();
 
             return $"Successfully imported {parts.Count}";
+        }
+
+        public static string ImportCars(CarDealerContext context, string inputXml)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("Cars");
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportCarDto[]), xmlRoot);
+
+            using StringReader stringReader = new StringReader(inputXml);
+
+            ImportCarDto[] carsDto = (ImportCarDto[])xmlSerializer.Deserialize(stringReader);
+
+            ICollection<Car> cars = new HashSet<Car>();
+
+            foreach(ImportCarDto carDto in carsDto)
+            {
+                Car car = new Car()
+                {
+                    Make = carDto.Make,
+                    Model = carDto.Model,
+                    TravelledDistance = carDto.TraveledDistance,
+                };
+
+                ICollection<PartCar> currentCarParts = new HashSet<PartCar>();
+                foreach (int partId in carDto.Parts.Select(p => p.Id).Distinct())
+                {
+                    Part part = context.Parts.Find(partId);
+
+                    if (part == null)
+                    {
+                        continue;
+                    }
+
+                    PartCar partCar = new PartCar()
+                    {
+                        Car = car,
+                        Part = part
+                    };
+                    currentCarParts.Add(partCar);
+                }
+                car.PartCars = currentCarParts;
+                cars.Add(car);
+            }
+
+            context.Cars.AddRange(cars);
+            context.SaveChanges();
+
+            return $"Successfully imported {cars.Count}";
         }
     }
 }
