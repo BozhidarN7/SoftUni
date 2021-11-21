77974b9d45cae6a9d7e5611783ca86c880772151
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-11-21 17:46:42 +0200

    Added another problem from XML-Processing-Exercises

---
+++ EntityFrameworkCore/09. XML-Processing-Exercises/CarDealer/DTO/ImportDto/ImportPartDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTO.ImportDto
+{
+    [XmlType("Part")]
+    public class ImportPartDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("price")]
+        public string Price { get; set; }
+
+        [XmlElement("quantity")]
+        public string Quantity { get; set; }
+        
+        [XmlElement("supplierId")]
+        public string SupplierId { get; set; }
+    }
+}
--- EntityFrameworkCore/09. XML-Processing-Exercises/CarDealer/StartUp.cs
+++ EntityFrameworkCore/09. XML-Processing-Exercises/CarDealer/StartUp.cs
@@ -1,8 +1,10 @@
 ﻿using CarDealer.Data;
 using CarDealer.DTO.ImportDto;
 using CarDealer.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Xml.Serialization;
 
 namespace CarDealer
@@ -16,8 +18,10 @@ namespace CarDealer
             context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
 
-            string supplierInput = File.ReadAllText("../../../Datasets/suppliers.xml");
-            System.Console.WriteLine(ImportSuppliers(context, supplierInput));
+            string suppliersInput = File.ReadAllText("../../../Datasets/suppliers.xml");
+            string partsInput = File.ReadAllText("../../../Datasets/parts.xml");
+            Console.WriteLine(ImportSuppliers(context, suppliersInput));
+            Console.WriteLine(ImportParts(context, partsInput));
         }
 
         public static string ImportSuppliers(CarDealerContext context, string inputXml)
@@ -30,7 +34,7 @@ namespace CarDealer
             ImportSupplierDto[] suppliersDto = (ImportSupplierDto[])xmlSerializer.Deserialize(stringReader);
 
             ICollection<Supplier> suppliers = new HashSet<Supplier>();
-            foreach(ImportSupplierDto supplierDto in suppliersDto)
+            foreach (ImportSupplierDto supplierDto in suppliersDto)
             {
                 Supplier supplier = new Supplier()
                 {
@@ -44,6 +48,40 @@ namespace CarDealer
             context.SaveChanges();
 
             return $"Successfully imported {suppliers.Count}";
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputXml)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("Parts");
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportPartDto[]), xmlRoot);
+
+            using StringReader stringReader = new StringReader(inputXml);
+
+            ImportPartDto[] partsDto = (ImportPartDto[])xmlSerializer.Deserialize(stringReader);
+
+            ICollection<Part> parts = new HashSet<Part>();
+            foreach (ImportPartDto partDto in partsDto)
+            {
+                if (!context.Suppliers.Any(s => s.Id == int.Parse(partDto.SupplierId)))
+                {
+                    continue;
+                }
+
+                Part part = new Part()
+                {
+                    Name = partDto.Name,
+                    Price = decimal.Parse(partDto.Price),
+                    Quantity = int.Parse(partDto.Quantity),
+                    SupplierId = int.Parse(partDto.SupplierId)
+                };
+
+                parts.Add(part);
+            }
+
+            context.Parts.AddRange(parts);
+            context.SaveChanges();
+
+            return $"Successfully imported {parts.Count}";
         }
     }
 }
