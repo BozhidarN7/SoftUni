6ba8d1a4ff911b1699eefc716d635d6219e38344
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-11-21 17:21:12 +0200

    Added the first problem from XML-Processing-Exercises

---
+++ EntityFrameworkCore/09. XML-Processing-Exercises/CarDealer/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using CarDealer.Data;
+using CarDealer.DTO.ImportDto;
+using CarDealer.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            string supplierInput = File.ReadAllText("../../../Datasets/suppliers.xml");
+            System.Console.WriteLine(ImportSuppliers(context, supplierInput));
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("Suppliers");
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportSupplierDto[]), xmlRoot);
+
+            using StringReader stringReader = new StringReader(inputXml);
+
+            ImportSupplierDto[] suppliersDto = (ImportSupplierDto[])xmlSerializer.Deserialize(stringReader);
+
+            ICollection<Supplier> suppliers = new HashSet<Supplier>();
+            foreach(ImportSupplierDto supplierDto in suppliersDto)
+            {
+                Supplier supplier = new Supplier()
+                {
+                    Name = supplierDto.Name,
+                    IsImporter = bool.Parse(supplierDto.IsImoprter)
+                };
+                suppliers.Add(supplier);
+            }
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}";
+        }
+    }
+}
