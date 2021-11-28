7df224b6a7f5dd4d7d2efdd8bc2b1c9e50c9c812
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-11-28 09:54:34 +0200

    Added the final part of the exam on 4 April 2021

--- EntityFrameworkCore/Exams/DatabasesAdvancedExam-4April2021/TeisterMask/DataProcessor/Serializer.cs
+++ EntityFrameworkCore/Exams/DatabasesAdvancedExam-4April2021/TeisterMask/DataProcessor/Serializer.cs
@@ -1,21 +1,87 @@
 ﻿namespace TeisterMask.DataProcessor
 {
     using System;
-
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
     using Data;
-
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+    using TeisterMask.DataProcessor.ExportDto;
     using Formatting = Newtonsoft.Json.Formatting;
 
     public class Serializer
     {
         public static string ExportProjectWithTheirTasks(TeisterMaskContext context)
         {
-            throw new NotImplementedException();
+            StringBuilder result = new StringBuilder();
+
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("Projects");
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ExportProjectDto[]), xmlRoot);
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using StringWriter sw = new StringWriter(result);
+
+            ExportProjectDto[] projects = context.Projects
+                .ToArray()
+                .Where(p => p.Tasks.Any())
+                .Select(p => new ExportProjectDto()
+                {
+                    ProjectName = p.Name,
+                    TasksCount = p.Tasks.Count,
+                    HasEndDate = p.DueDate == null ? "No" : "Yes",
+                    Tasks = p.Tasks
+                    .OrderBy(t => t.Name)
+                    .Select(t => new ExportProjectTaskDto()
+                    {
+                        Name = t.Name,
+                        Label = t.LabelType.ToString()
+                    })
+                    .ToArray()
+                })
+                .OrderByDescending(p => p.TasksCount)
+                .ThenBy(p => p.ProjectName)
+                .ToArray();
+
+            xmlSerializer.Serialize(sw, projects, namespaces);
+
+            return result.ToString().TrimEnd();
         }
 
         public static string ExportMostBusiestEmployees(TeisterMaskContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            var mostBusiestEmployees = context.Employees
+                .ToArray()
+                .Where(e => e.EmployeesTasks.Any(et => et.Task.OpenDate >= date))
+                .Select(e => new
+                {
+                    Username = e.Username,
+                    Tasks = e.EmployeesTasks
+                        .Where(et => et.Task.OpenDate >= date)
+                        .Select(et => et.Task)
+                        .OrderByDescending(t => t.DueDate)
+                        .ThenBy(t => t.Name)
+                        .Select(t => new
+                        {
+                            TaskName = t.Name,
+                            OpenDate = t.OpenDate.ToString("d", CultureInfo.InvariantCulture),
+                            DueDate = t.DueDate.ToString("d", CultureInfo.InvariantCulture),
+                            LabelType = t.LabelType.ToString(),
+                            ExecutionType = t.ExecutionType.ToString()
+                        })
+                        .ToArray()
+                })
+                .OrderByDescending(e => e.Tasks.Length)
+                .ThenBy(e => e.Username)
+                .Take(10)
+                .ToArray();
+
+            string json = JsonConvert.SerializeObject(mostBusiestEmployees, Formatting.Indented);
+
+            return json;
         }
     }
 }
