611feee71aa270b45590a26ddd3c641645bc63b3
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-11-28 12:55:22 +0200

    Added the first part from the exam on 08 August 2020

---
+++ EntityFrameworkCore/Exams/DatabasesAdvancedExam-08August2020/VaporStore/Data/Models/Card.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using VaporStore.Data.Models.Enums;
+
+namespace VaporStore.Data.Models
+{
+    public class Card
+    {
+        public Card()
+        {
+            Purchases = new HashSet<Purchase>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Number { get; set; }
+
+        [Required]
+        public string Cvc { get; set; }
+
+        public CardType Type { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public virtual ICollection<Purchase> Purchases { get; set; }
+    }
+}
