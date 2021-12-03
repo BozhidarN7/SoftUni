44c9a3b286843e636ef84eccc5a8c24761b31204
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-12-03 16:48:08 +0200

    Added the first part from the exam on 13 Dec 2019

---
+++ EntityFrameworkCore/Exams/C#DBAdvancedExam-13Dec2019/BookShop/Data/BookShopContext.cs
@@ -0,0 +1,35 @@
+﻿namespace BookShop.Data
+{
+    using BookShop.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class BookShopContext : DbContext
+    {
+        public BookShopContext() { }
+
+        public BookShopContext(DbContextOptions options)
+            : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<AuthorBook> AuthorsBooks { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<AuthorBook>(e =>
+            {
+                e.HasKey(ab => new { ab.AuthorId, ab.BookId });
+            });
+        }
+    }
+}
