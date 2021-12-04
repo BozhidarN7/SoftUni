25a9ac8c2fcbb96b9cc66bc24130cb8232e7d460
Bozhidar Nemski <75275295+BozhidarN7@users.noreply.github.com>
2021-12-04 12:37:40 +0200

    Added the final part from the exam on 13 Dec 2019

---
+++ EntityFrameworkCore/Exams/C#DBAdvancedExam-13Dec2019/BookShop/DataProcessor/ExportDto/ExportBookDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace BookShop.DataProcessor.ExportDto
+{
+    [XmlType("Book")]
+    public class ExportBookDto
+    {
+        [XmlAttribute("Pages")]
+        public string Pages { get; set; }
+
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("Date")]
+        public string Date { get; set; }
+    }
+}
--- EntityFrameworkCore/Exams/C#DBAdvancedExam-13Dec2019/BookShop/DataProcessor/Serializer.cs
+++ EntityFrameworkCore/Exams/C#DBAdvancedExam-13Dec2019/BookShop/DataProcessor/Serializer.cs
@@ -7,6 +7,7 @@
     using System.Text;
     using System.Xml;
     using System.Xml.Serialization;
+    using BookShop.DataProcessor.ExportDto;
     using Data;
     using Newtonsoft.Json;
     using Formatting = Newtonsoft.Json.Formatting;
@@ -15,12 +16,57 @@
     {
         public static string ExportMostCraziestAuthors(BookShopContext context)
         {
-            throw new NotImplementedException();
+            var authors = context.Authors
+                .ToArray()
+                .Select(a => new
+                {
+                    AuthorName = $"{a.FirstName} {a.LastName}",
+                    Books = a.AuthorsBooks
+                    .Select(ab => ab.Book)
+                    .OrderByDescending(b => b.Price)
+                    .Select(ab => new
+                    {
+                        BookName = ab.Name,
+                        BookPrice = ab.Price.ToString("f2"),
+                    })
+                    .ToArray()
+                })
+                .OrderByDescending(a => a.Books.Length)
+                .ThenBy(a => a.AuthorName)
+                .ToArray();
+
+            string json = JsonConvert.SerializeObject(authors, Formatting.Indented);
+            return json;
         }
 
         public static string ExportOldestBooks(BookShopContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("Books");
+            XmlSerializer serializer = new XmlSerializer(typeof(ExportBookDto[]), xmlRoot);
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            ExportBookDto[] books = context.Books
+                .ToArray()
+                .Where(b => b.PublishedOn < date && b.Genre.ToString() == "Science")
+                .OrderByDescending(b => b.Pages)
+                .ThenByDescending(b => b.PublishedOn)
+                .Select(b => new ExportBookDto()
+                {
+                    Pages = b.Pages.ToString(),
+                    Name = b.Name,
+                    Date = b.PublishedOn.ToString("d", CultureInfo.InvariantCulture)
+                })
+                .Take(10)
+                .ToArray();
+
+            StringBuilder result = new StringBuilder();
+            using (StringWriter sw = new StringWriter(result))
+            {
+                serializer.Serialize(sw, books, namespaces);
+            }
+
+            return result.ToString().TrimEnd();
         }
     }
 }
